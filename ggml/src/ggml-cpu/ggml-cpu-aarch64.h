73d33b0b13f6d14a4921553ad81b7067994a559e
SXX <sxx1136965276@gmail.com>
2025-04-21 22:23:30 +0800

    ggml: Implement yield barrier using futex for improved thread scheduling efficiency

--- ggml/src/ggml-cpu/ggml-cpu-aarch64.h
+++ ggml/src/ggml-cpu/ggml-cpu-aarch64.h
@@ -6,3 +6,15 @@
 // GGML internal header
 
 ggml_backend_buffer_type_t ggml_backend_cpu_aarch64_buffer_type(void);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(GGML_YIELD_BARRIER)
+size_t ggml_barrier_spin_count(unsigned int n_threads);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
