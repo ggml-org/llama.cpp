795e8dc82922ac46a2850083dc8f6dfacf6f3e3e
Gabe Goodhart <ghart@us.ibm.com>
2025-05-20 15:46:24 -0600

    feat: Add bamba to llama_arch_is_hybrid_recurrent
    Branch: GraniteFour
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- src/llama-arch.cpp
+++ src/llama-arch.cpp
@@ -1819,6 +1819,8 @@ bool llm_arch_is_hybrid_recurrent(const llm_arch & arch) {
     // TODO: There are currently no hybrid models! Once there are, this will be
     //  the place to identify them
     switch (arch) {
+        case LLM_ARCH_BAMBA:
+            return true;
         default:
             return false;
     }
