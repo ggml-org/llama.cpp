528dd51f5a10a2289ef8a1d7f23e923955506e7f
Johannes Gäßler <johannesg@5d6.de>
2025-05-24 16:29:47 +0200

    WIP

--- ggml/src/ggml-backend.cpp
+++ ggml/src/ggml-backend.cpp
@@ -1427,14 +1427,14 @@ static void ggml_backend_sched_split_graph(ggml_backend_sched_t sched, struct gg
             GGML_ASSERT(sched->splits != NULL);
         }
         sched->splits[i_split] = splits_tp[i_split];
-        fprintf(stderr, "%s: split%d backend_id=%d i_start=%d i_end=%d\n",
-            __func__, int(i_split), sched->splits[i_split].backend_id, sched->splits[i_split].i_start, sched->splits[i_split].i_end);
-        for (int n = 0; n < sched->splits[i_split].n_inputs; n++) {
-            fprintf(stderr, "%s:   - input %d: %s\n", __func__, n, sched->splits[i_split].inputs[n]->name);
-        }
-        for (int n = 0; n < sched->splits[i_split].graph.n_nodes; n++) {
-            fprintf(stderr, "%s:   - node %d: %s\n", __func__, n, sched->splits[i_split].graph.nodes[n]->name);
-        }
+        // fprintf(stderr, "%s: split%d backend_id=%d i_start=%d i_end=%d\n",
+        //     __func__, int(i_split), sched->splits[i_split].backend_id, sched->splits[i_split].i_start, sched->splits[i_split].i_end);
+        // for (int n = 0; n < sched->splits[i_split].n_inputs; n++) {
+        //     fprintf(stderr, "%s:   - input %d: %s\n", __func__, n, sched->splits[i_split].inputs[n]->name);
+        // }
+        // for (int n = 0; n < sched->splits[i_split].graph.n_nodes; n++) {
+        //     fprintf(stderr, "%s:   - node %d: %s\n", __func__, n, sched->splits[i_split].graph.nodes[n]->name);
+        // }
     }
     sched->n_splits = splits_tp.size();
 
