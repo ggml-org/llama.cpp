1b71c2319652760a31cc02c7c3139de09d3a83b1
Ervin Tasnadi <etasnadi@protonmail.com>
2025-05-27 17:16:08 +0200

    Missing barrier added to shader. Number of additional tests reduced to 108.

--- tests/test-backend-ops.cpp
+++ tests/test-backend-ops.cpp
@@ -4031,8 +4031,8 @@ static std::vector<std::unique_ptr<test_case>> make_test_cases_eval() {
 
     for(uint32_t Cout : {1, 9}){
         for(uint32_t Cin : {1, 7}){
-            for(uint32_t K : {1, 2, 3, 5, 6, 8, 9, 28}){
-                for(uint32_t L : {1, 2, 3, 5, 15, 16, 60, 100, 111, 127, 128, 157, 255, 376, 1024, 1173}){
+            for(uint32_t K : {1, 3, 1337}){
+                for(uint32_t L : {1, 2, 2173}){
                     for(uint32_t s0: {1, 2, 3}){
                         test_cases.emplace_back(new test_conv_transpose_1d({L,Cin,1,1}, {K,Cout,Cin,1}, s0, 0, 1));
                     }
