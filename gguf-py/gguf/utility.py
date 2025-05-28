2c51593549dfa9d32e4045d4adb7b812c68c238a
Beinsezii <beinsezii@gmail.com>
2025-05-27 22:47:24 -0700

    gguf/utility: return full content on size < 0

--- gguf-py/gguf/utility.py
+++ gguf-py/gguf/utility.py
@@ -231,7 +231,7 @@ class SafetensorRemote:
         response.raise_for_status()
 
         # Get raw byte data
-        return response.content[:size]
+        return response.content[:size] if size > -1 else response.content
 
     @classmethod
     def check_file_exist(cls, url: str) -> bool:
