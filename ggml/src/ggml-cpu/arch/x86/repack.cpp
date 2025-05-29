df2781034bdbfeb7b1a8c0511288d30ae4599f66
Xiongchuan Tan <xc-tan@outlook.com>
2025-05-30 01:20:02 +0800

    try to fix clang compilation errors, 3rd attempt

--- ggml/src/ggml-cpu/arch/x86/repack.cpp
+++ ggml/src/ggml-cpu/arch/x86/repack.cpp
@@ -1,21 +1,14 @@
 #define GGML_COMMON_IMPL_CPP
 #define GGML_COMMON_DECL_CPP
 #include "ggml-common.h"
-#include "ggml-backend-impl.h"
-
-#include "ggml-quants.h"
 #include "ggml-impl.h"
-#include "ggml-cpu.h"
-#include "ggml-cpu-impl.h"
-#include "traits.h"
 
-#include <cmath>
 #include <cstring>
 #include <cassert>
-#include <cfloat>
 #include <cstdlib> // for qsort
 #include <cstdio>  // for GGML_ASSERT
 
+#define GGML_CPU_CLANG_WORKAROUND
 #include "../../repack.h"
 
 #if defined(__GNUC__)
