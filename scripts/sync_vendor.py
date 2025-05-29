72bcf195e5a91989d6c25c912221e992f58c1d29
Georgi Gerganov <ggerganov@gmail.com>
2025-05-29 22:39:22 +0300

    cont : fix lint

--- scripts/sync_vendor.py
+++ scripts/sync_vendor.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import urllib.request
+
 vendor = {
     "https://github.com/nlohmann/json/releases/latest/download/json.hpp": "common/nlohmann/json.hpp",
 
     # sync manually
-    #"https://raw.githubusercontent.com/ochafik/minja/refs/heads/main/include/minja/minja.hpp":         "common/minja/minja.hpp",
-    #"https://raw.githubusercontent.com/ochafik/minja/refs/heads/main/include/minja/chat-template.hpp": "common/minja/chat-template.hpp",
+    # "https://raw.githubusercontent.com/ochafik/minja/refs/heads/main/include/minja/minja.hpp":         "common/minja/minja.hpp",
+    # "https://raw.githubusercontent.com/ochafik/minja/refs/heads/main/include/minja/chat-template.hpp": "common/minja/chat-template.hpp",
 
     "https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image.h": "tools/mtmd/vendor/stb_image.h",
 
@@ -14,9 +16,6 @@ vendor = {
     "https://raw.githubusercontent.com/yhirose/cpp-httplib/refs/tags/v0.20.1/httplib.h": "tools/server/httplib.h",
 }
 
-import urllib.request
-
 for url, filename in vendor.items():
-    print(f"downloading {url} to {filename}")
+    print(f"downloading {url} to {filename}") # noqa: NP100
     urllib.request.urlretrieve(url, filename)
-
