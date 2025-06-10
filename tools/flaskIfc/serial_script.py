9688963ed09199949b6c38dad67a1b061b38f933
Ashish Trivedi <atrivedi@fpga2.tsavoritesi.net>
2025-06-10 11:33:49 -0700

    @FIR-715: Added FlaskInterface tool for serial port This is a first version of FlaskInterface tool with following 1. Xterm Interface via Browser via /terminal endpoint 2. Serial console interface via Browser via /serial endpoint

---
+++ tools/flaskIfc/serial_script.py
@@ -0,0 +1,44 @@
+import serial
+import sys
+
+def send_serial_command(port, baudrate, command):
+    try:
+        # Open the serial port with 1 second timeout
+        ser = serial.Serial(port, baudrate, timeout=1)
+
+        ser.write(command.encode())  # Encode command to bytes
+        ser.write('\n'.encode())  # Encode command to bytes
+        
+        # Wait to read the serial port
+        data = '\0'
+        while True:
+            try:
+                line = ser.readline()
+                if line: # Check if line is not empty
+                    data += (line.decode('utf-8').strip()) # Decode and strip to remove extra chars
+                else:
+                    break  # Exit loop if no data is received
+            except serial.SerialException as e:
+                ser.close()
+                return (f"Error reading from serial port: {e}")
+            except KeyboardInterrupt:
+                ser.close()
+                return ("Program interrupted by user")
+        ser.close()
+        return data
+
+    except serial.SerialException as e:
+        ser.close()
+        return f"Error: {e}"
+
+# This script can be run in standalone as well
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: python script.py <port> <baudrate> <command>")
+        sys.exit(1)
+
+    port = sys.argv[1]
+    baudrate = int(sys.argv[2])
+    command = sys.argv[3]
+    response = send_serial_command(port, baudrate, command)
+    print(response)
