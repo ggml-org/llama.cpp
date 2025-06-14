80dc7e69e20088fe20fbfee9f186e275ab86d14e
Xiongchuan Tan <xc-tan@outlook.com>
2025-06-14 15:00:38 +0800

    fix ppc detection

--- ggml/src/ggml-cpu/apple-fallback.h
+++ ggml/src/ggml-cpu/apple-fallback.h
@@ -64,7 +64,7 @@
 #define ggml_gemm_q4_0_4x4_q8_0_generic ggml_gemm_q4_0_4x4_q8_0
 #define ggml_gemm_q4_0_4x8_q8_0_generic ggml_gemm_q4_0_4x8_q8_0
 #define ggml_gemm_iq4_nl_4x4_q8_0_generic ggml_gemm_iq4_nl_4x4_q8_0
-#elif defined(__powerpc__)
+#elif defined(__PPC__)
 // quants.c
 #define quantize_row_q8_K_generic quantize_row_q8_K
 #define ggml_vec_dot_tq1_0_q8_K_generic ggml_vec_dot_tq1_0_q8_K
