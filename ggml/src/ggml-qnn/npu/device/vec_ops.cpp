813672d9ac98ae12b5085063863bb947b9e50db3
hongruichen <chraac@gmail.com>
2025-06-21 01:30:10 +0800

    fix test failure at width == 193

--- ggml/src/ggml-qnn/npu/device/vec_ops.cpp
+++ ggml/src/ggml-qnn/npu/device/vec_ops.cpp
@@ -170,43 +170,47 @@ inline float vec_dot_product_mixed_impl(const _TElem0 * src0, const _TElem1 * sr
         sum1 = _AddFunc(_MpyFunc(Q6_V_hi_W(s0_pair), h1), sum1);
     }
 
-    sum                        = _AddFunc(sum0, sum1);
-    bool src0_low_vec_consumed = false;
-    if (src1_vec_ptr_end - src1_vec_ptr > 0) {
-        const bool should_fetch_src0 =
-            reinterpret_cast<const _TElem0 *>(hexagon::align_down(src0_vec_ptr)) < src0_ptr_end;
-        HVX_Vector curr0 = should_fetch_src0 ? *src0_vec_ptr : prev0;
-        HVX_Vector curr1 = *src1_vec_ptr++;
-        src0_vec_ptr += should_fetch_src0 ? 1 : 0;
-        HVX_Vector     s0      = Q6_V_valign_VVR(curr0, prev0, (size_t) src0);
-        HVX_Vector     s1      = Q6_V_valign_VVR(curr1, prev1, (size_t) src1);
-        HVX_VectorPair s0_pair = _ExpandFunc(s0);
-        prev1                  = curr1;
+    sum                                = _AddFunc(sum0, sum1);
+    const size_t leftover1             = count % kElementsPerVector1;
+    bool         src0_low_vec_consumed = false;
+    {
+        HVX_Vector curr0 = Q6_V_vzero();
+        if (src1_vec_ptr_end - src1_vec_ptr > 0) {
+            const bool should_fetch_src0 =
+                reinterpret_cast<const _TElem0 *>(hexagon::align_down(src0_vec_ptr)) < src0_ptr_end;
+            curr0            = should_fetch_src0 ? *src0_vec_ptr : prev0;
+            HVX_Vector curr1 = *src1_vec_ptr++;
+            src0_vec_ptr += should_fetch_src0 ? 1 : 0;
+            HVX_Vector     s0      = Q6_V_valign_VVR(curr0, prev0, (size_t) src0);
+            HVX_Vector     s1      = Q6_V_valign_VVR(curr1, prev1, (size_t) src1);
+            HVX_VectorPair s0_pair = _ExpandFunc(s0);
+            prev1                  = curr1;
 
-        sum = _AddFunc(_MpyFunc(Q6_V_lo_W(s0_pair), s1), sum);  // TODO: figure out how to handle the high part
-        src0_low_vec_consumed = true;
+            sum = _AddFunc(_MpyFunc(Q6_V_lo_W(s0_pair), s1), sum);  // TODO: figure out how to handle the high part
+            src0_low_vec_consumed = true;
+        }
+
+        if ((src1_vec_ptr_end - ((HVX_Vector *) src1)) > 0) {
+            // handle the last vector
+            const bool should_fetch_src0 =
+                !src0_low_vec_consumed &&
+                reinterpret_cast<const _TElem0 *>(hexagon::align_down(src0_vec_ptr)) < src0_ptr_end;
+            bool should_fetch_src1 = leftover1 != 0 || !hexagon::is_addr_aligned(src1_vec_ptr);
+            curr0                  = should_fetch_src0 ? *src0_vec_ptr : curr0;
+            HVX_Vector curr1       = should_fetch_src1 ? *src1_vec_ptr : prev1;
+            src0_vec_ptr += should_fetch_src0 ? 1 : 0;
+            src1_vec_ptr += should_fetch_src1 ? 1 : 0;
+            HVX_Vector     s0      = Q6_V_valign_VVR(curr0, prev0, (size_t) src0);
+            HVX_Vector     s1      = Q6_V_valign_VVR(curr1, prev1, (size_t) src1);
+            HVX_VectorPair s0_pair = _ExpandFunc(s0);
+            prev0                  = curr0;
+            prev1                  = curr1;
+
+            sum = _AddFunc(_MpyFunc(src0_low_vec_consumed ? Q6_V_hi_W(s0_pair) : Q6_V_lo_W(s0_pair), s1), sum);
+        }
     }
 
-    const size_t leftover0 = count % kElementsPerVector0;
-    const size_t leftover1 = count % kElementsPerVector1;
-    if ((src1_vec_ptr_end - ((HVX_Vector *) src1)) > 0) {
-        // handle the last vector
-        const bool should_fetch_src0 = !src0_low_vec_consumed && reinterpret_cast<const _TElem0 *>(
-                                                                     hexagon::align_down(src0_vec_ptr)) < src0_ptr_end;
-        bool       should_fetch_src1 = leftover1 != 0 || !hexagon::is_addr_aligned(src1_vec_ptr);
-        HVX_Vector curr0             = should_fetch_src0 ? *src0_vec_ptr : prev0;
-        HVX_Vector curr1             = should_fetch_src1 ? *src1_vec_ptr : prev1;
-        src0_vec_ptr += should_fetch_src0 ? 1 : 0;
-        src1_vec_ptr += should_fetch_src1 ? 1 : 0;
-        HVX_Vector     s0      = Q6_V_valign_VVR(curr0, prev0, (size_t) src0);
-        HVX_Vector     s1      = Q6_V_valign_VVR(curr1, prev1, (size_t) src1);
-        HVX_VectorPair s0_pair = _ExpandFunc(s0);
-        prev0                  = curr0;
-        prev1                  = curr1;
-
-        sum = _AddFunc(_MpyFunc(src0_low_vec_consumed ? Q6_V_hi_W(s0_pair) : Q6_V_lo_W(s0_pair), s1), sum);
-    }
-
+    const size_t leftover0       = count % kElementsPerVector0;
     const size_t leftover_bytes1 = leftover1 * sizeof(_TElem1);
     if (leftover1 > 0) {
         // handle the leftover elements
