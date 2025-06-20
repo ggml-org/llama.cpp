5530bec16eeeb4089f074e216f39fe75d310fe87
Aaron Teo <aaron.teo1@ibm.com>
2025-06-21 01:55:24 +0800

    ggml-cpu: add sigint for gdb to break
    Signed-off-by: Aaron Teo <aaron.teo1@ibm.com>

--- ggml/src/ggml-cpu/ggml-cpu.c
+++ ggml/src/ggml-cpu/ggml-cpu.c
@@ -3143,6 +3143,7 @@ void ggml_cpu_fp32_to_fp16(const float * x, ggml_fp16_t * y, int64_t n) {
         float32x4_t v_x1 = vec_xl(0, x + i + 0);
         float32x4_t v_x2 = vec_xl(0, x + i + 4);
         uint16x8_t v_dlf16 = vec_round_from_fp32(v_x1, v_x2, 0);
+        raise(SIGINT);
         vec_xst(v_dlf16, 0, (uint16_t *)(y + i));
     }
     // TODO: Enable bottom code once checks are done
