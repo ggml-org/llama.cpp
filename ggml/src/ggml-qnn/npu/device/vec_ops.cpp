228dbd3b3c108040f3c8bbd92c23cd3f0d460d9b
hongruichen <chraac@gmail.com>
2025-06-30 18:00:07 +0800

    Optimize vec_dot_product_mix_aligned_impl for improved performance with additional vector sums

--- ggml/src/ggml-qnn/npu/device/vec_ops.cpp
+++ ggml/src/ggml-qnn/npu/device/vec_ops.cpp
@@ -239,8 +239,28 @@ inline float vec_dot_product_mix_aligned_impl(const _TElem0 * src0, const _TElem
     HVX_Vector * const src1_vec_ptr_end = ((HVX_Vector *) src1) + count / kElementsPerVector1;
     HVX_Vector         sum0             = Q6_V_vzero();
     HVX_Vector         sum1             = Q6_V_vzero();
+    HVX_Vector         sum2             = Q6_V_vzero();
+    HVX_Vector         sum3             = Q6_V_vzero();
 
-    while (src1_vec_ptr_end - src1_vec_ptr > 1) {
+    while (src1_vec_ptr_end - src1_vec_ptr > 3) {
+        HVX_VectorPair curr0  = reinterpret_cast<HVX_VectorPair *>(src0_vec_ptr)[0];
+        HVX_VectorPair curr10 = reinterpret_cast<HVX_VectorPair *>(src1_vec_ptr)[0];
+        HVX_VectorPair curr11 = reinterpret_cast<HVX_VectorPair *>(src1_vec_ptr)[1];
+
+        HVX_VectorPair s00 = _ExpandFunc(Q6_V_lo_W(curr0));
+        HVX_VectorPair s01 = _ExpandFunc(Q6_V_hi_W(curr0));
+        src0_vec_ptr += 2;
+        src1_vec_ptr += 4;
+
+        sum0 = _AddFunc(_MpyFunc(Q6_V_lo_W(s00), Q6_V_lo_W(curr10)), sum0);
+        sum1 = _AddFunc(_MpyFunc(Q6_V_hi_W(s00), Q6_V_hi_W(curr10)), sum1);
+        sum2 = _AddFunc(_MpyFunc(Q6_V_lo_W(s01), Q6_V_lo_W(curr11)), sum2);
+        sum3 = _AddFunc(_MpyFunc(Q6_V_hi_W(s01), Q6_V_hi_W(curr11)), sum3);
+    }
+
+    sum0 = _AddFunc(sum0, sum2);
+    sum1 = _AddFunc(sum1, sum3);
+    if (src1_vec_ptr_end - src1_vec_ptr > 1) {
         HVX_Vector     curr0 = src0_vec_ptr[0];
         HVX_VectorPair curr1 = reinterpret_cast<HVX_VectorPair *>(src1_vec_ptr)[0];
 
