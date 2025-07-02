eaec9c68207547d54f4ce3e18f5f4112c116cba4
Gabe Goodhart <ghart@us.ibm.com>
2025-07-02 11:58:41 -0600

    fix: Fix bad merge in tensor_mapping.py w/ SSM_NORM
    Branch: GraniteFour
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- gguf-py/gguf/tensor_mapping.py
+++ gguf-py/gguf/tensor_mapping.py
@@ -588,10 +588,6 @@ class TensorNameMap:
             "model.layers.{bid}.mamba.norm",     # bamba
         ),
 
-        MODEL_TENSOR.SSM_NORM: (
-            "backbone.layers.{bid}.mixer.norm",  # mamba2
-        ),
-
         MODEL_TENSOR.SSM_OUT: (
             "model.layers.{bid}.out_proj",
             "backbone.layers.{bid}.mixer.out_proj",
