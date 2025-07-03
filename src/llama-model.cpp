09012b87f038d0de137db643eb91fe4ef811b941
Georgi Gerganov <ggerganov@gmail.com>
2025-07-03 15:10:47 +0300

    llama : add "virtual sequences"
    ggml-ci

--- src/llama-model.cpp
+++ src/llama-model.cpp
@@ -15927,6 +15927,7 @@ llama_memory_i * llama_model::create_memory(const llama_memory_params & params, 
                                 params.swa_full,
                                 cparams.n_ctx,
                                 cparams.n_seq_max,
+                                cparams.n_seq_virt,
                                 cparams.n_ubatch,
                                 padding);
                     } else {
@@ -15941,6 +15942,7 @@ llama_memory_i * llama_model::create_memory(const llama_memory_params & params, 
                                 cparams.offload_kqv,
                                 cparams.n_ctx,
                                 cparams.n_seq_max,
+                                cparams.n_seq_virt,
                                 padding,
                                 hparams.n_swa,
                                 hparams.swa_type);
