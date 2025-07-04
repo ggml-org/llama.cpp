51c53ae8f26f104b1a6d087a3373667aa1c7dfd1
hongruichen <chraac@gmail.com>
2025-07-04 09:14:15 +0800

    wip

--- ggml/src/ggml-qnn/npu/device/vec_ops.cpp
+++ ggml/src/ggml-qnn/npu/device/vec_ops.cpp
@@ -248,27 +248,31 @@ inline float vec_dot_product_mix_aligned_impl(const _TElem0 * src0, const _TElem
     HVX_Vector * const src1_vec_ptr_end = ((HVX_Vector *) src1) + count / kElementsPerVector1;
     HVX_Vector         sum0             = Q6_V_vzero();
     HVX_Vector         sum1             = Q6_V_vzero();
-    HVX_Vector         sum2             = Q6_V_vzero();
-    HVX_Vector         sum3             = Q6_V_vzero();
 
-    while (src1_vec_ptr_end - src1_vec_ptr > 3) {
-        HVX_VectorPair curr0  = reinterpret_cast<HVX_VectorPair *>(src0_vec_ptr)[0];
-        HVX_VectorPair curr10 = reinterpret_cast<HVX_VectorPair *>(src1_vec_ptr)[0];
-        HVX_VectorPair curr11 = reinterpret_cast<HVX_VectorPair *>(src1_vec_ptr)[1];
+    {
+        HVX_Vector sum2 = Q6_V_vzero();
+        HVX_Vector sum3 = Q6_V_vzero();
 
-        HVX_VectorPair curr00 = _ExpandFunc(Q6_V_lo_W(curr0), kOneV);
-        HVX_VectorPair curr01 = _ExpandFunc(Q6_V_hi_W(curr0), kOneV);
-        src0_vec_ptr += 2;
-        src1_vec_ptr += 4;
+        while (src1_vec_ptr_end - src1_vec_ptr > 3) {
+            HVX_VectorPair curr0  = reinterpret_cast<HVX_VectorPair *>(src0_vec_ptr)[0];
+            HVX_VectorPair curr10 = reinterpret_cast<HVX_VectorPair *>(src1_vec_ptr)[0];
+            HVX_VectorPair curr11 = reinterpret_cast<HVX_VectorPair *>(src1_vec_ptr)[1];
 
-        sum0 = _AddFunc(_MpyFunc(Q6_V_lo_W(curr00), Q6_V_lo_W(curr10)), sum0);
-        sum1 = _AddFunc(_MpyFunc(Q6_V_hi_W(curr00), Q6_V_hi_W(curr10)), sum1);
-        sum2 = _AddFunc(_MpyFunc(Q6_V_lo_W(curr01), Q6_V_lo_W(curr11)), sum2);
-        sum3 = _AddFunc(_MpyFunc(Q6_V_hi_W(curr01), Q6_V_hi_W(curr11)), sum3);
+            HVX_VectorPair curr00 = _ExpandFunc(Q6_V_lo_W(curr0), kOneV);
+            HVX_VectorPair curr01 = _ExpandFunc(Q6_V_hi_W(curr0), kOneV);
+            src0_vec_ptr += 2;
+            src1_vec_ptr += 4;
+
+            sum0 = _AddFunc(_MpyFunc(Q6_V_lo_W(curr00), Q6_V_lo_W(curr10)), sum0);
+            sum1 = _AddFunc(_MpyFunc(Q6_V_hi_W(curr00), Q6_V_hi_W(curr10)), sum1);
+            sum2 = _AddFunc(_MpyFunc(Q6_V_lo_W(curr01), Q6_V_lo_W(curr11)), sum2);
+            sum3 = _AddFunc(_MpyFunc(Q6_V_hi_W(curr01), Q6_V_hi_W(curr11)), sum3);
+        }
+
+        sum0 = _AddFunc(sum0, sum2);
+        sum1 = _AddFunc(sum1, sum3);
     }
 
-    sum0 = _AddFunc(sum0, sum2);
-    sum1 = _AddFunc(sum1, sum3);
     if (src1_vec_ptr_end - src1_vec_ptr > 1) {
         HVX_Vector     curr0 = src0_vec_ptr[0];
         HVX_VectorPair curr1 = reinterpret_cast<HVX_VectorPair *>(src1_vec_ptr)[0];
