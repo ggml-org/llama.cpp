b9b532cc267be43f608f325caa94faa0b732cb53
Ryan Mangeno <160974989+ryan-mangeno@users.noreply.github.com>
2025-07-09 19:15:15 -0400

    Update gguf-py/gguf/tensor_mapping.py
    Co-authored-by: Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>

--- gguf-py/gguf/tensor_mapping.py
+++ gguf-py/gguf/tensor_mapping.py
@@ -450,8 +450,7 @@ class TensorNameMap:
             "model.layers.h.{bid}.mlp.c_proj",                        # exaone
             "model.layers.{bid}.feed_forward.down_proj",              # llama4 jamba
             "transformer_encoder.{bid}.ffn.w3",                       # neobert
-            "blk.{bid}.ffn_down",                                     # smoldocling                                     
-
+            "blk.{bid}.ffn_down",                                     # smoldocling
         ),
 
         MODEL_TENSOR.FFN_DOWN_EXP: (
