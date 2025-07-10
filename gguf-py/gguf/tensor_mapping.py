310eb46400cf1216abe39289eda427d3db10f64c
Ryan Mangeno <160974989+ryan-mangeno@users.noreply.github.com>
2025-07-10 10:51:03 -0400

    Update gguf-py/gguf/tensor_mapping.py
    Co-authored-by: Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>

--- gguf-py/gguf/tensor_mapping.py
+++ gguf-py/gguf/tensor_mapping.py
@@ -252,8 +252,8 @@ class TensorNameMap:
         ),
 
         MODEL_TENSOR.ATTN_POST_NORM: (
-            "model.layers.{bid}.post_attention_layernorm",                   # gemma2 olmo2    # ge
-            "model.layers.{bid}.post_self_attn_layernorm",                   # glm-4-0414
+            "model.layers.{bid}.post_attention_layernorm",     # gemma2 olmo2    # ge
+            "model.layers.{bid}.post_self_attn_layernorm",     # glm-4-0414
         ),
 
         # Rotary embeddings
