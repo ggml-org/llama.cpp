35436eaa12433341a16199717f9e73f060a62386
Tianyue-Zhao <zhaotianyue@outlook.com>
2025-07-10 00:27:01 +0000

    Added CogVLM vision model to conversion script

--- convert_hf_to_gguf.py
+++ convert_hf_to_gguf.py
@@ -8306,6 +8306,29 @@ class SmallThinkerModel(TextModel):
 
 
 @ModelBase.register("CogVLMForCausalLM")
+class CogVLMVisionModel(MmprojModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.hparams_vision['num_attention_heads'] = self.hparams['num_heads']
+    
+    def modify_tensors(self, data_torch: Tensor, name: str, bid: int | None) -> Iterable[tuple[str, Tensor]]:
+        del bid  # unused
+
+        if not name.startswith("model.vision."):
+            return []
+
+        if "query_key_value" in name:
+            # Split tensor into three along first axis
+            q, k, v = data_torch.split(data_torch.shape[0] // 3, dim=0)
+            return [
+                (self.map_tensor_name(name.replace("query_key_value", "query")), q),
+                (self.map_tensor_name(name.replace("query_key_value", "key")), k),
+                (self.map_tensor_name(name.replace("query_key_value", "value")), v),
+            ]
+        
+        return [(self.map_tensor_name(name), data_torch)]
+
+@ModelBase.register("CogVLMForCausalLM")
 class CogVLMModel(LlamaModel):
     model_arch = gguf.MODEL_ARCH.COGVLM
     
@@ -8319,7 +8342,7 @@ class CogVLMModel(LlamaModel):
         if name.startswith("model.vision."):
             return []
         
-        if "query_key_value" in name:
+        if "query_key_value.weight" in name:
             # Slice tensor into three along first axis
             q, k, v = data_torch.split(data_torch.shape[0] // 3, dim=0)
             return [
