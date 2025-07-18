Fix indentation in EXAONE4 rope scaling

convert_hf_to_gguf.py
@@ -6386,8 +6386,10 @@ class ExaoneModel(TextModel):
                     elif wavelen > low_freq_wavelen:
                         rope_factors.append(factor)
                     else:
-                        smooth = (old_context_len / wavelen - low_freq_factor) / (high_freq_factor - low_freq_factor)
-                rope_factors.append(1 / ((1 - smooth) / factor + smooth))
+                        smooth = (
+                            old_context_len / wavelen - low_freq_factor
+                        ) / (high_freq_factor - low_freq_factor)
+                        rope_factors.append(1 / ((1 - smooth) / factor + smooth))
 
                 yield (self.format_tensor_name(gguf.MODEL_TENSOR.ROPE_FREQS), torch.tensor(rope_factors, dtype=torch.float32))
 