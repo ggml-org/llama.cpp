41e243b1a3c0676bdc2d3ba200579aa4887b0741
Johannes Gäßler <johannesg@5d6.de>
2025-07-29 09:26:34 +0200

    Update scripts/server-bench.py
    Co-authored-by: Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>

--- scripts/server-bench.py
+++ scripts/server-bench.py
@@ -69,7 +69,7 @@ def get_server(path_server: str, path_log: Optional[str]) -> dict:
             sleep(1.0)
             exit_code = process.poll()
             if exit_code is not None:
-                raise RuntimeError(f"llama.cpp server exited unexpectedly with exit code {exit_code}, see {path_log.format(port=port)}")
+                raise RuntimeError(f"llama.cpp server exited unexpectedly with exit code {exit_code}{path_log and f', see {path_log.format(port=port)}' or ''}")
             response = requests.get(f"{address}/health")
             if response.status_code == 200:
                 break
