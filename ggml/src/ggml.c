b00126aef847112256d4ec61f6e8bd398db61b09
David Sanftenberg <d.sanftenberg@cardano.com>
2025-07-29 21:41:04 +0100

    undo cleverness

--- ggml/src/ggml.c
+++ ggml/src/ggml.c
@@ -1151,14 +1151,8 @@ static const char * GGML_GLU_OP_NAME[GGML_GLU_OP_COUNT] = {
 static_assert(GGML_GLU_OP_COUNT == 5, "GGML_GLU_OP_COUNT != 5");
 
 
-#define GGML_ASSERT_ALIGNED_MSG_P(N, A, P, MSG) \
-    static_assert((N) % (A) == 0, MSG " (size=" #N ", align=" #A ", padding=" #P ")")
-#define GGML_ASSERT_ALIGNED_MSG(N, A, MSG) \
-    GGML_ASSERT_ALIGNED_MSG_P(N, A, ((A) - (N) % (A)) % (A), MSG)
-
-// check that the tensor and object sizes are multiples of GGML_MEM_ALIGN
-GGML_ASSERT_ALIGNED_MSG(sizeof(struct ggml_object), GGML_MEM_ALIGN, "ggml_object size must be a multiple of GGML_MEM_ALIGN");
-GGML_ASSERT_ALIGNED_MSG(sizeof(struct ggml_tensor), GGML_MEM_ALIGN, "ggml_tensor size must be a multiple of GGML_MEM_ALIGN");
+static_assert(sizeof(struct ggml_object)%GGML_MEM_ALIGN == 0, "ggml_object size must be a multiple of GGML_MEM_ALIGN");
+static_assert(sizeof(struct ggml_tensor)%GGML_MEM_ALIGN == 0, "ggml_tensor size must be a multiple of GGML_MEM_ALIGN");
 
 
 ////////////////////////////////////////////////////////////////////////////////
