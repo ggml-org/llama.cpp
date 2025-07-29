ff97352641f1137f712697acd228efadced813fa
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2025-07-29 19:40:00 +0900

    common/chat.cpp: skip debug log if there is a risk of unfinished UTF-8 sequence at the end

--- common/chat.cpp
+++ common/chat.cpp
@@ -1944,7 +1944,8 @@ common_chat_msg common_chat_parse(const std::string & input, bool is_partial, co
         }
     }
     auto msg = builder.result();
-    msg.content = truncate_incomplete_utf8(msg.content);
-    LOG_DBG("Parsed message: %s\n", common_chat_msgs_to_json_oaicompat<json>({msg}).at(0).dump().c_str());
+    if (msg.content == truncate_incomplete_utf8(msg.content)) {
+        LOG_DBG("Parsed message: %s\n", common_chat_msgs_to_json_oaicompat<json>({msg}).at(0).dump().c_str());
+    }
     return msg;
 }
