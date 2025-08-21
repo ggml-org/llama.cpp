a0fc2a20a2b986f869832cfc1dba5538e25fc668
Jesse CreateThis <jesse@createthis.com>
2025-08-21 15:59:40 +0000

    Add DeepSeek-V3.1

--- convert_hf_to_gguf.py
+++ convert_hf_to_gguf.py
@@ -851,6 +851,9 @@ class TextModel(ModelBase):
         if chkhsh == "b3f499bb4255f8ca19fccd664443283318f2fd2414d5e0b040fbdd0cc195d6c5":
             # ref: https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
             res = "deepseek-r1-qwen"
+        if chkhsh == "877081d19cf6996e2c4ff0e1236341e9b7bde288f5311a56a937f0afbbb3aeb5":
+            # ref: https://huggingface.co/deepseek-ai/DeepSeek-V3.1
+            res = "deepseek-v3.1"
         if chkhsh == "ccc2ef013c104be7bae2965776d611e1d7a8a2a9c547dd93a682c9a9fc80352e":
             # ref: https://huggingface.co/Xenova/gpt-4o
             res = "gpt-4o"
