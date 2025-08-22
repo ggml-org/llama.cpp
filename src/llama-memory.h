2c2fbbd6dc43e971fe7f5d1639bb2311abacde4a
Georgi Gerganov <ggerganov@gmail.com>
2025-08-22 14:36:07 +0300

    cont : update comments [no ci]

--- src/llama-memory.h
+++ src/llama-memory.h
@@ -68,8 +68,8 @@ struct llama_memory_i {
     // this callback is used to filter out layers that should not be included in the cache
     using layer_filter_cb = std::function<bool(int32_t il)>;
 
-    // this callback is used to specify which layers should reuse KV cache from other layers
-    // return negative value to indicate that the layer il should not reuse KV cache
+    // this callback is used to specify which layers should reuse memory from other layers
+    // return negative value to indicate that the layer il should not reuse memory
     using layer_reuse_cb = std::function<int32_t(int32_t il)>;
 
     virtual ~llama_memory_i() = default;
