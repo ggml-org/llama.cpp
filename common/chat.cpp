79f7ca3eef9faf8c596d3a49e51c9887ea7344a8
Jesse CreateThis <jesse@createthis.com>
2025-08-23 15:34:08 +0000

    Revert "Try using try_find_literal instead of regex"
    This reverts commit c50d887ec2780dd9e6b8b397e92347d3db8d5575.

--- common/chat.cpp
+++ common/chat.cpp
@@ -1352,7 +1352,8 @@ static void common_chat_parse_deepseek_v3_1(common_chat_msg_parser & builder) {
     // There's no opening "<think>" tag, so we need to handle this differently
     
     // First, try to find the "</think>" tag that separates thinking from regular content
-    if (auto res = builder.try_find_literal("</think>")) {
+    static const common_regex thinking_end_regex("</think>");
+    if (auto res = builder.try_find_regex(thinking_end_regex, std::string::npos, false)) {
         // The prelude contains everything before the "</think>" tag
         auto stripped_reasoning = string_strip(res->prelude);
         
@@ -1360,7 +1361,7 @@ static void common_chat_parse_deepseek_v3_1(common_chat_msg_parser & builder) {
             builder.add_reasoning_content(stripped_reasoning);
         }
         
-        // The parser position is already advanced past the "</think>" tag by try_find_literal
+        // The parser position is already advanced past the "</think>" tag by try_find_regex
         // The rest is regular content
         builder.add_content(builder.consume_rest());
     } else {
