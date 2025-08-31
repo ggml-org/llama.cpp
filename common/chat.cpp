0f7bfaf679b4bdc9b3e6b99e3a496b3004cf6123
Piotr Wilkin <piotr.wilkin@syndatis.com>
2025-08-31 14:40:47 +0200

    Whitespace

--- common/chat.cpp
+++ common/chat.cpp
@@ -2145,7 +2145,7 @@ static void common_chat_parse_nemotron_v2(common_chat_msg_parser & builder) {
         } else {
             throw common_chat_msg_partial_exception("Incomplete tool call");
         }
-    } 
+    }
     builder.add_content(builder.consume_rest());
 }
 
