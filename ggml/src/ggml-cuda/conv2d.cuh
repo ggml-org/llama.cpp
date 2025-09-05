96db6275398f6a9301c206d1ffdf302dae853a90
mnehete32 <mnehete32@gmail.com>
2025-09-05 11:50:52 +0530

    CUDA: conv2d added comment

--- ggml/src/ggml-cuda/conv2d.cuh
+++ ggml/src/ggml-cuda/conv2d.cuh
@@ -5,6 +5,8 @@ constexpr int BS_OC     = 128;
 constexpr int BS_ICKHKW = 16;
 constexpr int BS_NOHOW  = 128;
 
+// supported configuration
+// https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#element-types-and-matrix-sizes
 constexpr int WMMA_M = 16;
 constexpr int WMMA_N = 16;
 constexpr int WMMA_K = 16;
