10af12c3f08eca12753f56a7c8831fe5009a7a2c
Shunta Saito <shunta.saito@gmail.com>
2025-09-19 00:18:12 +0900

    Support old GGUF converted by the previous version of llama.cpp

--- src/llama-model.cpp
+++ src/llama-model.cpp
@@ -1079,8 +1079,8 @@ void llama_model::load_hparams(llama_model_loader & ml) {
                }
 
                // Load attention parameters
-               ml.get_key(LLM_KV_ATTENTION_KEY_LENGTH,   hparams.n_embd_head_k);
-               ml.get_key(LLM_KV_ATTENTION_VALUE_LENGTH, hparams.n_embd_head_v);
+               ml.get_key(LLM_KV_ATTENTION_KEY_LENGTH,   hparams.n_embd_head_k, false);
+               ml.get_key(LLM_KV_ATTENTION_VALUE_LENGTH, hparams.n_embd_head_v, false);
             } break;
         case LLM_ARCH_GPT2:
             {
