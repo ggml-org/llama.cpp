42b38373e78dd3dc54d4960d2777cb94c2b2b7aa
Gabe Goodhart <ghart@us.ibm.com>
2025-09-26 16:43:39 -0600

    feat(llama-gguf): Print out the tensor type in llama-gguf r
    Branch: Mamba2Perf
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- examples/gguf/gguf.cpp
+++ examples/gguf/gguf.cpp
@@ -184,8 +184,9 @@ static bool gguf_ex_read_1(const std::string & fname, bool check_data) {
             const char * name   = gguf_get_tensor_name  (ctx, i);
             const size_t size   = gguf_get_tensor_size  (ctx, i);
             const size_t offset = gguf_get_tensor_offset(ctx, i);
+            const char * type   = ggml_type_name(gguf_get_tensor_type(ctx, i));
 
-            printf("%s: tensor[%d]: name = %s, size = %zu, offset = %zu\n", __func__, i, name, size, offset);
+            printf("%s: tensor[%d]: name = %s, size = %zu, offset = %zu, type = %s\n", __func__, i, name, size, offset, type);
         }
     }
 
