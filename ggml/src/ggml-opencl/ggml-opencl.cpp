057e67c85d1137da6f9882cff9faaa284067028c
Li He <lih@qti.qualcomm.com>
2025-10-09 21:46:25 -0700

    opencl: use q8_0 mm for larger matrix

--- ggml/src/ggml-opencl/ggml-opencl.cpp
+++ ggml/src/ggml-opencl/ggml-opencl.cpp
@@ -6980,6 +6980,9 @@ static void ggml_cl_mul_mat(ggml_backend_t backend, const ggml_tensor * src0, co
                 return;
             }
             case GGML_TYPE_Q8_0: {
+                if (ne11 < 32) {
+                    break;
+                }
                 kernel = backend_ctx->kernel_mul_mm_q8_0_f32_l4_lm;
                 nth0 = 128; // calculated as (BM*BN)/(TM*TN)
 
