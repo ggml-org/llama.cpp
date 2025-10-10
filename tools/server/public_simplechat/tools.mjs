bbaae70503d56fc24dc2618beee951b12eae20c0
hanishkvc <hanishkvc@gmail.com>
2025-10-11 00:02:32 +0530

    SimpleChatTC: More generic tooljs, SimpCalc, some main skeleton
    Make tooljs structure and flow more generic
    
    Add a simple_calculator tool/function call logic
    
    Add initial skeleton wrt the main tools.mjs file.

--- tools/server/public_simplechat/tools.mjs
+++ tools/server/public_simplechat/tools.mjs
@@ -1,7 +1,29 @@
 //@ts-check
-// Helpers to handle tools/functions calling
+// DANGER DANGER DANGER - Simple and Stupid - Use from a discardable VM only
+// Helpers to handle tools/functions calling in a direct and dangerous way
 // by Humans for All
 //
 
 
 import * as tjs from './tooljs.mjs'
+
+
+/**
+ * @type {Object<string,Object>}
+ */
+let tc_switch = {}
+
+export function setup() {
+    for (const key in tjs.tc_switch) {
+        tc_switch[key] = tjs.tc_switch[key]
+    }
+}
+
+export function meta() {
+    let tools = []
+    for (const key in tc_switch) {
+        tools.push(tc_switch[key]["meta"])
+    }
+    return tools
+}
+
