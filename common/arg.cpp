e571f14c2b9e1b36a69354f2fe4b37e1d31ec5ca
Georgi Gerganov <ggerganov@gmail.com>
2025-10-11 10:11:34 +0300

    cont : remove reasoning overrides [no ci]

--- common/arg.cpp
+++ common/arg.cpp
@@ -4062,7 +4062,7 @@ common_params_context common_params_parser_init(common_params & params, llama_ex
             params.sampling.top_k = 0;
             params.sampling.min_p = 0.01f;
             params.use_jinja = true;
-            params.default_template_kwargs["reasoning_effort"] = "\"high\"";
+            //params.default_template_kwargs["reasoning_effort"] = "\"high\"";
         }
     ).set_examples({LLAMA_EXAMPLE_SERVER}));
 
@@ -4081,7 +4081,7 @@ common_params_context common_params_parser_init(common_params & params, llama_ex
             params.sampling.top_k = 0;
             params.sampling.min_p = 0.01f;
             params.use_jinja = true;
-            params.default_template_kwargs["reasoning_effort"] = "\"high\"";
+            //params.default_template_kwargs["reasoning_effort"] = "\"high\"";
         }
     ).set_examples({LLAMA_EXAMPLE_SERVER}));
 
