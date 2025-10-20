33741e0061360c4de715373e844465982295f864
Aleksander Grygier <aleksander.grygier@gmail.com>
2025-10-20 11:34:26 +0200

    feat: Improve configuration

--- tools/server/webui/playwright.config.ts
+++ tools/server/webui/playwright.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
 	webServer: {
 		command: 'npm run build && npx http-server ../public -p 8181',
-		port: 8181
+		port: 8181,
+		timeout: 120000,
+		reuseExistingServer: false
 	},
 	testDir: 'e2e'
 });
