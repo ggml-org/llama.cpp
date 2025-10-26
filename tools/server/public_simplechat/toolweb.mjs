0407c78271e36156da33b29fee5818f653da0522
hanishkvc <hanishkvc@gmail.com>
2025-10-26 15:54:47 +0530

    SimpleChatTC:ToolCalling:Seprat out JSWebWorker and ProxyBasedWeb
    Remove the unneed (belonging to the other file) stuff from tooljs
    and toolweb files.
    
    Update tools manager to make use of the new toolweb module

--- tools/server/public_simplechat/toolweb.mjs
+++ tools/server/public_simplechat/toolweb.mjs
@@ -1,75 +1,11 @@
 //@ts-check
-// DANGER DANGER DANGER - Simple and Stupid - Use from a discardable VM only
-// Helpers to handle tools/functions calling wrt
-// * javascript interpreter
-// * simple arithmatic calculator
+// ALERT - Simple Stupid flow - Using from a discardable VM is better
+// Helpers to handle tools/functions calling related to web access
 // by Humans for All
 //
 
 
 let gToolsWorker = /** @type{Worker} */(/** @type {unknown} */(null));
-
-
-let js_meta = {
-        "type": "function",
-        "function": {
-            "name": "run_javascript_function_code",
-            "description": "Runs given code using eval within a web worker context in a browser's javascript environment and returns the console.log outputs of the execution after few seconds",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "code": {
-                        "type": "string",
-                        "description": "The code that will be run using eval within a web worker in the browser's javascript interpreter environment."
-                    }
-                },
-                "required": ["code"]
-            }
-        }
-    }
-
-
-/**
- * Implementation of the javascript interpretor logic. Minimal skeleton for now.
- * ALERT: Has access to the javascript web worker environment and can mess with it and beyond
- * @param {string} toolcallid
- * @param {string} toolname
- * @param {any} obj
- */
-function js_run(toolcallid, toolname, obj) {
-    gToolsWorker.postMessage({ id: toolcallid, name: toolname, code: obj["code"]})
-}
-
-
-let calc_meta = {
-        "type": "function",
-        "function": {
-            "name": "simple_calculator",
-            "description": "Calculates the provided arithmatic expression using console.log within a web worker of a browser's javascript interpreter environment and returns the output of the execution once it is done in few seconds",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "arithexpr":{
-                        "type":"string",
-                        "description":"The arithmatic expression that will be calculated by passing it to console.log of a browser's javascript interpreter."
-                    }
-                },
-                "required": ["arithexpr"]
-            }
-        }
-    }
-
-
-/**
- * Implementation of the simple calculator logic. Minimal skeleton for now.
- * ALERT: Has access to the javascript web worker environment and can mess with it and beyond
- * @param {string} toolcallid
- * @param {string} toolname
- * @param {any} obj
- */
-function calc_run(toolcallid, toolname, obj) {
-    gToolsWorker.postMessage({ id: toolcallid, name: toolname, code: `console.log(${obj["arithexpr"]})`})
-}
 
 
 /**
@@ -229,29 +165,17 @@ async function fetchweburltext_setup(tcs) {
 
 
 /**
- * @type {Object<string, Object<string, any>>}
- */
-export let tc_switch = {
-    "run_javascript_function_code": {
-        "handler": js_run,
-        "meta": js_meta,
-        "result": ""
-    },
-    "simple_calculator": {
-        "handler": calc_run,
-        "meta": calc_meta,
-        "result": ""
-    },
-}
-
-
-/**
  * Used to get hold of the web worker to use for running tool/function call related code
  * Also to setup tool calls, which need to cross check things at runtime
  * @param {Worker} toolsWorker
  */
 export async function init(toolsWorker) {
+    /**
+     * @type {Object<string, Object<string, any>>} tcs
+     */
+    let tc_switch = {}
     gToolsWorker = toolsWorker
     await fetchweburlraw_setup(tc_switch)
     await fetchweburltext_setup(tc_switch)
+    return tc_switch
 }
