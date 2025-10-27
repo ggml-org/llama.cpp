edb8c0f4a708223f6dd10c527337d9da1ee27281
Mason M <mason@bandoti.com>
2025-10-26 21:11:20 -0300

    Write to log when enabled otherwise direct

--- common/console.h
+++ common/console.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <string>
+#include "log.h"
 
 namespace console {
     enum display_t {
@@ -16,9 +17,27 @@ namespace console {
     void init(bool use_simple_io, bool use_advanced_display);
     void cleanup();
     void set_display(display_t display);
+    display_t    get_display();
+    const char * get_display_color();
     bool readline(std::string & line, bool multiline_input);
 
-    void write(const char* format, ...);
+    void write_console(const char* format, ...);
+
+    template<typename... Args>
+    void write(const char* format, Args... args) {
+        if (get_display() == user_input || !common_log_is_active(common_log_main())) {
+            write_console(format, args...);
+
+        } else {
+            const char * color = get_display_color();
+            std::string colored_format = std::string(color) + format + LOG_COL_DEFAULT;
+            common_log_add(common_log_main(), GGML_LOG_LEVEL_CONT, colored_format.c_str(), args...);
+        }
+    }
+
+    inline void write(const char* data) {
+        write("%s", data);
+    }
 
     inline void write(const std::string & data) {
         write("%s", data.c_str());
