ac54c7165269846a4ce5c36a41776569840b00f2
ddh0 <chemist-mulches-39@icloud.com>
2025-11-04 23:32:34 -0600

    add `glm4v` and `glm4v_moe` to src/models

---
+++ src/models/glm4v.cpp
@@ -0,0 +1,5 @@
+#include "models.h"
+
+llm_build_glm4v::llm_build_glm4v(const llama_model & model, const llm_graph_params & params) : llm_graph_context(params) {
+ // TODO
+}
---
+++ src/models/glm4v_moe.cpp
@@ -0,0 +1,5 @@
+#include "models.h"
+
+llm_build_glm4v_moe::llm_build_glm4v_moe(const llama_model & model, const llm_graph_params & params) : llm_graph_context(params) {
+ // TODO
+}
