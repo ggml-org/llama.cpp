e21f4997326de57b3a42756f872722128e3214d4
chraac <chraac@gmail.com>
2025-11-06 19:41:42 +0800

    feat: optimize binary job function with fast division and modulo calculations

--- ggml/src/ggml-hexagon/htp/binary-ops.c
+++ ggml/src/ggml-hexagon/htp/binary-ops.c
@@ -114,14 +114,27 @@ static void binary_job_f32_per_thread(const struct htp_tensor * src0,
 
     const uint8_t * restrict data_src1 = (const uint8_t *) src1->data;
 
+    const uint32_t ne0201 = ne02 * ne01;
+    uint32_t       div03[2];
+    uint32_t       div02[2];
+    init_fastdiv_values(ne0201, &div03[0], &div03[1]);
+    init_fastdiv_values(ne01, &div02[0], &div02[1]);
+
+    uint32_t mod13[2];
+    uint32_t mod12[2];
+    uint32_t mod11[2];
+    init_fastdiv_values(ne13, &mod13[0], &mod13[1]);
+    init_fastdiv_values(ne12, &mod12[0], &mod12[1]);
+    init_fastdiv_values(ne11, &mod11[0], &mod11[1]);
+
     for (uint32_t ir = src0_start_row; ir < src0_end_row; ir++) {
-        const uint32_t i03 = ir / (ne02 * ne01);
-        const uint32_t i02 = (ir - i03 * ne02 * ne01) / ne01;
+        const uint32_t i03 = fastdiv(ir, div03[0], div03[1]);
+        const uint32_t i02 = fastdiv(ir - i03 * ne02 * ne01, div02[0], div02[1]);
         const uint32_t i01 = (ir - i03 * ne02 * ne01 - i02 * ne01);
 
-        const uint32_t i13 = i03 % ne13;
-        const uint32_t i12 = i02 % ne12;
-        const uint32_t i11 = i01 % ne11;
+        const uint32_t i13 = fastmodulo(i03, ne13, mod13[0], mod13[1]);
+        const uint32_t i12 = fastmodulo(i02, ne12, mod12[0], mod12[1]);
+        const uint32_t i11 = fastmodulo(i01, ne11, mod11[0], mod11[1]);
 
         const uint8_t * restrict src1_ptr = data_src1 + i13 * nb13 + i12 * nb12 + i11 * src1_row_size;
 
