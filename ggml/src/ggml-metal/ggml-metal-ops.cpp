f2e6508b237d14960effe950139b685bb84b5a62
Xi Guo <37179170+xi-guo-0@users.noreply.github.com>
2025-11-16 12:14:16 +0800

    fix: Metal Flash Attention assertion failure

--- ggml/src/ggml-metal/ggml-metal-ops.cpp
+++ ggml/src/ggml-metal/ggml-metal-ops.cpp
@@ -2153,7 +2153,8 @@ int ggml_metal_op_flash_attn_ext(ggml_metal_op_t ctx, int idx) {
 
         bool need_sync = false;
 
-        const bool has_kvpad = ne11 % ncpsg != 0;
+        // const bool has_kvpad = ne11 % ncpsg != 0;
+        const bool has_kvpad = true;
 
         if (has_kvpad) {
             assert(ggml_metal_op_flash_attn_ext_extra_pad(op) != 0);
@@ -2325,7 +2326,8 @@ int ggml_metal_op_flash_attn_ext(ggml_metal_op_t ctx, int idx) {
 
         bool need_sync = false;
 
-        const bool has_kvpad = ne11 % ncpsg != 0;
+        // const bool has_kvpad = ne11 % ncpsg != 0;
+        const bool has_kvpad = true;
 
         if (has_kvpad) {
             assert(ggml_metal_op_flash_attn_ext_extra_pad(op) != 0);
