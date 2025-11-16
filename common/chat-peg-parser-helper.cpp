09976dda104fe13bd38ba48ad9c10d111974f89b
Piotr Wilkin <piotr.wilkin@syndatis.com>
2025-11-16 12:31:30 +0100

    Change + to append()

--- common/chat-peg-parser-helper.cpp
+++ common/chat-peg-parser-helper.cpp
@@ -2,7 +2,11 @@
 #include "chat-peg-parser.h"
 
 common_chat_peg_parser common_chat_peg_parser_builder_helper::reasoning(const std::string &tag) {
-    return add_rule("raw-reasoning", std::string("<" + tag + ">") << add_rule("reasoning-content", until("</" + tag + ">")) << "</" + tag + ">");
+    std::string open_tag;
+    open_tag.append("<").append(tag).append(">");
+    std::string close_tag;
+    close_tag.append("</").append(tag).append(">");
+    return add_rule("raw-reasoning", open_tag << add_rule("reasoning-content", until(close_tag)) << close_tag);
 }
 
 common_chat_peg_parser common_chat_peg_parser_builder_helper::content_before_tools(const std::string &tag) {
@@ -14,21 +18,62 @@ common_chat_peg_parser common_chat_peg_parser_builder_helper::quasi_xml_no_attr(
     std::vector<common_chat_peg_parser> args;
 
     for (auto it = parameters.begin(); it != parameters.end(); it++) {
-        auto arg_name = add_rule(std::string("arg-start-" + *it), literal("<" + param_tag + "=" + *it + ">"));
-        auto arg_end = add_rule("arg-end", "</" + param_tag + ">" + peek(literal("<" + param_tag + "=") | ("</" + function_tag + ">")));
+        std::string arg_start_name;
+        arg_start_name.append("arg-start-").append(*it);
+
+        std::string param_open;
+        param_open.append("<").append(param_tag).append("=").append(*it).append(">");
+
+        auto arg_name = add_rule(arg_start_name, literal(param_open));
+
+        std::string param_close_end;
+        param_close_end.append("</").append(param_tag).append(">");
+
+        std::string param_close_peek;
+        param_close_peek.append("</").append(function_tag).append(">");
+
+        std::string param_peek_open;
+        param_peek_open.append("<").append(param_tag).append("=");
+        auto arg_end = add_rule("arg-end", param_close_end + peek(literal(param_peek_open) | param_close_peek));
+
+        std::string string_content_1;
+        string_content_1.append("</").append(param_tag).append("><").append(param_tag).append("=");
+
+        std::string string_content_2;
+        string_content_2.append("</").append(param_tag).append("></").append(function_tag).append(">");
+
         auto string_arg_content = add_rule("arg-string-content",
-            until_one_of({"</" + param_tag + "><" + param_tag + "=", "</" + param_tag + "></" + function_tag + ">"}));
-        auto string_arg = add_rule("arg-string-" + *it, arg_name + string_arg_content + arg_end);
-        auto json_sec = json();
-        auto json_arg = add_rule("arg-json-" + *it, arg_name + add_rule("arg-json-content", json_sec) + arg_end);
+            until_one_of({string_content_1, string_content_2}));
+
+        std::string arg_string_name;
+        arg_string_name.append("arg-string-").append(*it);
+        auto string_arg = add_rule(arg_string_name, arg_name + string_arg_content + arg_end);
+auto json_sec = json();
+
+        std::string arg_json_name;
+        arg_json_name.append("arg-json-").append(*it);
+        auto json_arg = add_rule(arg_json_name, arg_name + add_rule("arg-json-content", json_sec) + arg_end);
         auto arg_json_or_string = one_or_more(json_arg | string_arg);
         args.push_back(arg_json_or_string);
-    }
 
-    auto args_sequence = sequence(args);
-    auto function = add_rule("function-" + function_name,
-                add_rule("function-start-" + function_name, "<" + function_tag + "=" + function_name + ">")
-                + args_sequence + "</" + function_tag + ">");
+    }
+auto args_sequence = sequence(args);
+
+    std::string function_start_name;
+    function_start_name.append("function-start-").append(function_name);
+
+    std::string function_open;
+    function_open.append("<").append(function_tag).append("=").append(function_name).append(">");
+
+    std::string function_close;
+    function_close.append("</").append(function_tag).append(">");
+
+    std::string function_rule_name;
+    function_rule_name.append("function-").append(function_name);
+    auto function = add_rule(function_rule_name,
+
+                add_rule(function_start_name, function_open)
+                + args_sequence + function_close);
 
     return function;
 }
