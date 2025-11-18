552e5b2b61a719fa7146db24bda44e3e40ea1199
Phylliida Dev <phylliida.dev@gmail.com>
2025-11-18 13:25:03 -0800

    Update test-backend-ops.cpp

--- tests/test-backend-ops.cpp
+++ tests/test-backend-ops.cpp
@@ -5490,6 +5490,16 @@ struct test_pad_ext : public test_case {
 
 // GGML_OP_PAD_REFLECT_1D
 
+static inline int64_t wrap_coord_circular(int64_t coord, int64_t size) {
+    GGML_ASSERT(size > 0);
+    const int64_t mod = coord % size;
+    return mod < 0 ? mod + size : mod;
+}
+
+static inline int64_t offset4d(const int64_t ne[4], int64_t i0, int64_t i1, int64_t i2, int64_t i3) {
+    return ((i3 * ne[2] + i2) * ne[1] + i1) * ne[0] + i0;
+}
+
 struct test_pad_ext_circular : public test_case {
     const std::array<int64_t, 4> ne_src{4, 3, 1, 1};
     const std::array<int64_t, 4> pads_l{1, 2, 0, 0};
