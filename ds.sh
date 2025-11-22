d52d54125ed5cf6e032d3a16ebdb56cf5192624e
Jetson Tan <1809733265@qq.com>
2025-11-22 09:45:41 +0800

    更新 ds.sh

--- ds.sh
+++ ds.sh
@@ -1,3 +1,9 @@
 chmod -R +x build/bin/
 
+cd build/bin/
+
+wget https://www.modelscope.cn/models/unsloth/DeepSeek-R1-Distill-Qwen-1.5B-GGUF/resolve/master/DeepSeek-R1-Distill-Qwen-1.5B-Q4_K_M.gguf && mv DeepSeek-R1-Distill-Qwen-1.5B-Q4_K_M.gguf ds1.5b.gguf
+
+cd ../..
+
 ./build/bin/llama-cli -m ./build/bin/ds1.5b.gguf -n 256 -c 2048 --color -t $(nproc)
