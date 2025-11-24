d88ba1813cdf5e6893c546965c37c4187f944144
Daniel Bevenius <daniel.bevenius@gmail.com>
2025-11-24 09:31:14 +0100

    common : remove build-info.cpp from commit [no ci]
    This file was generated during the build process and should not be
    included in previous commits.

--- common/build-info.cpp
+++
@@ -1,4 +0,0 @@
-int LLAMA_BUILD_NUMBER = 5590;
-char const *LLAMA_COMMIT = "0d398442";
-char const *LLAMA_COMPILER = "cc (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0";
-char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
