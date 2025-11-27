e2e1e2542219bd3168968848ddf7a84e8c426ee2
Xiongchuan Tan <xc-tan@outlook.com>
2025-11-27 23:20:00 +0800

    fix linkage warning

--- ggml/src/ggml-cpu/arch/riscv/rvv-init.cpp
+++ ggml/src/ggml-cpu/arch/riscv/rvv-init.cpp
@@ -8,4 +8,6 @@ static int ggml_get_riscv_v_kernel_idx() {
     return vlen / 128;
 }
 
-extern "C" int ggml_rvv_kernel_idx = ggml_get_riscv_v_kernel_idx();
+extern "C" {
+    int ggml_rvv_kernel_idx = ggml_get_riscv_v_kernel_idx();
+}
