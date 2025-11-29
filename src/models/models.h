e0ca150100d1c91ee12779c1e2c856116ce099f4
ryan-mangeno <ryanmangeno@gmail.com>
2025-11-29 11:17:31 -0500

    modern bert put into seperate src file

--- src/models/models.h
+++ src/models/models.h
@@ -322,6 +322,11 @@ struct llm_build_minimax_m2 : public llm_graph_context {
     llm_build_minimax_m2(const llama_model & model, const llm_graph_params & params);
 };
 
+template <bool iswa>
+struct llm_build_modern_bert : public llm_graph_context {
+    llm_build_modern_bert(const llama_model & model, const llm_graph_params & params);
+};
+
 struct llm_build_mpt : public llm_graph_context {
     llm_build_mpt(const llama_model & model, const llm_graph_params & params);
 };
