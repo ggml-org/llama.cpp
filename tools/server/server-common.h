048231dda6918a655e03bf55d41555cf94555228
Itiua <valeriybushenev@gmail.com>
2025-11-29 19:11:25 +0300

    Add fork label to version output

--- tools/server/server-common.h
+++ tools/server/server-common.h
@@ -13,7 +13,9 @@
 #include <vector>
 #include <cinttypes>
 
-const static std::string build_info("b" + std::to_string(LLAMA_BUILD_NUMBER) + "-" + LLAMA_COMMIT);
+#define DEFAULT_OAICOMPAT_MODEL "gpt-3.5-turbo"
+
+const static std::string build_info("b" + std::to_string(LLAMA_BUILD_NUMBER) + "-" + LLAMA_COMMIT + " [itiu fork]");
 
 using json = nlohmann::ordered_json;
 
