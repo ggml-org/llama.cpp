9faca5a9fb5b9a4693ada56b9fb5a3bbc8d66c49
Johannes Gäßler <johannesg@5d6.de>
2025-12-14 11:22:24 +0100

    fix llama-fit-params verbosity

--- tools/fit-params/fit-params.cpp
+++ tools/fit-params/fit-params.cpp
@@ -24,7 +24,7 @@ int main(int argc, char ** argv) {
     auto cparams = common_context_params_to_llama(params);
     llama_params_fit(params.model.path.c_str(), &mparams, &cparams,
         params.tensor_split, params.tensor_buft_overrides.data(), params.fit_params_target, params.fit_params_min_ctx,
-        params.verbosity > 0 ? GGML_LOG_LEVEL_DEBUG : GGML_LOG_LEVEL_ERROR);
+        params.verbosity >= 4 ? GGML_LOG_LEVEL_DEBUG : GGML_LOG_LEVEL_ERROR);
 
     LOG_INF("Printing fitted CLI arguments to stdout...\n");
     std::cout << "-c "    << cparams.n_ctx;
