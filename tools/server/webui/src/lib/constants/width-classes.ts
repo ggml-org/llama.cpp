c638bfeb21fcda1ab037dd66aa9a851daa17a418
Imad Saddik <simad3647@gmail.com>
2025-12-15 22:02:42 +0100

    Updated the MAX_WIDTH_CLASSES constant

--- tools/server/webui/src/lib/constants/width-classes.ts
+++ tools/server/webui/src/lib/constants/width-classes.ts
@@ -1,4 +1,4 @@
 export const MAX_WIDTH_CLASSES =
-	'max-w-[48rem] md:max-w-[60rem] min-[1920px]:max-w-[80rem] min-[2560px]:max-w-[120rem]';
+	'max-w-[48rem] md:max-w-[60rem] xl:max-w-[70rem] 2xl:max-w-[80rem] 3xl:max-w-[90rem] 4xl:max-w-[100rem] 5xl:max-w-[150rem]';
 
 export const DEFAULT_MAX_WIDTH_CLASS = 'max-w-[48rem]';
