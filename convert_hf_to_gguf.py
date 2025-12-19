d7356bff3d521a93faa31dc1531172bd11c81ede
Ryan Mangeno <160974989+ryan-mangeno@users.noreply.github.com>
2025-12-18 21:13:26 -0500

    Update convert_hf_to_gguf.py
    Co-authored-by: Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>

--- convert_hf_to_gguf.py
+++ convert_hf_to_gguf.py
@@ -9924,7 +9924,7 @@ class ModernBertModel(BertModel):
         self.gguf_writer.add_dense_every_n_layers(self.hparams["global_attn_every_n_layers"])
         self.gguf_writer.add_sliding_window(self.hparams["local_attention"])
         self.gguf_writer.add_rope_freq_base(self.hparams["global_rope_theta"])
-        self.gguf_writer.add_rope_freq_base_swa(self.hparams["local_rope_theta"])
+        self.gguf_writer.add_rope_freq_base_swa(self.rope_parameters.get("sliding_attention", {"rope_theta": self.hparams.get("local_rope_theta")})["rope_theta"])
         self.gguf_writer.add_rope_scaling_type(gguf.RopeScalingType.NONE)
         self.gguf_writer.add_vocab_size(self.hparams["vocab_size"])
 
