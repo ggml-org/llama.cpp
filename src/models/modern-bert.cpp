bc3fd573ddd221f81c3152b412e733fd6df0140c
Ryan Mangeno <160974989+ryan-mangeno@users.noreply.github.com>
2025-12-22 10:10:10 -0500

    Update src/models/modern-bert.cpp
    Co-authored-by: Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>

--- src/models/modern-bert.cpp
+++ src/models/modern-bert.cpp
@@ -4,9 +4,6 @@ template <bool iswa>
 llm_build_modern_bert<iswa>::llm_build_modern_bert(const llama_model & model, const llm_graph_params & params) : llm_graph_context(params) {
     const int64_t n_embd_head     = hparams.n_embd_head_v;
     const int64_t n_embd_gqa      = hparams.n_embd_v_gqa();
-    const float rope_theta_global = hparams.rope_freq_base_train;
-    const float rope_theta_local  = hparams.rope_freq_base_train_swa;
-    const uint32_t swa_period  = hparams.swa_period;
 
     GGML_ASSERT(n_embd_head == hparams.n_embd_head_k);
 
