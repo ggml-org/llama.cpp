b75c39127609061756e3c2ed85b1f4f0b476bb8f
Jiacheng (Jason) Chen <76919340+jiachengjason@users.noreply.github.com>
2025-12-28 18:38:25 -0500

    Patch perf regression for mmq kernels in ROCm
    recover performance regression for https://github.com/ggml-org/llama.cpp/issues/17917

--- ggml/src/ggml-cuda/mmq.cu
+++ ggml/src/ggml-cuda/mmq.cu
@@ -333,7 +333,13 @@ bool ggml_cuda_should_use_mmq(enum ggml_type type, int cc, int64_t ne11, int64_t
     }
 
     if (amd_wmma_available(cc)) {
-        return true;
+        if (ne11 <= 128 || type == GGML_TYPE_Q4_0 || type == GGML_TYPE_Q4_1 || type == GGML_TYPE_Q5_0 || type == GGML_TYPE_Q5_1) {
+            return true;
+        }
+        if (ne11 <= 256 && (type == GGML_TYPE_Q4_K || type == GGML_TYPE_Q5_K)) {
+            return true;
+        }
+        return false;
     }
 
     return (!GGML_CUDA_CC_IS_CDNA(cc)) || ne11 < MMQ_DP4A_MAX_BATCH_SIZE;
