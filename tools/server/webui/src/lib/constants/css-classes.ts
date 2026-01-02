bf679e0a098644bfb7a3724c208f904f30c0bdf5
Aleksander Grygier <aleksander.grygier@gmail.com>
2026-01-02 19:37:40 +0100

    refactor: Consolidate UI CSS classes into shared module

---
+++ tools/server/webui/src/lib/constants/css-classes.ts
@@ -0,0 +1,9 @@
+export const BOX_BORDER =
+	'border border-border/30 focus-within:border-border  dark:border-border/20 dark:focus-within:border-border';
+
+export const INPUT_CLASSES = `
+    bg-muted/70 dark:bg-muted/85
+    {BOX_BORDER}
+    outline-none
+    text-foreground
+`;
