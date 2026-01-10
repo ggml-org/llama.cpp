2a666f5b28aae3e11d0920255f47d2bf01127e17
zhang hui <you@example.com>
2026-01-10 12:21:20 +0800

    adjust fattn policy

--- ggml/src/ggml-cuda/fattn.cu
+++ ggml/src/ggml-cuda/fattn.cu
@@ -317,7 +317,7 @@ static best_fattn_kernel ggml_cuda_get_best_fattn_kernel(const int device, const
     if (amd_wmma_available(cc) && GGML_CUDA_CC_IS_RDNA4(cc) && Q->ne[0] != 40 && Q->ne[0] != 72) {
         if (can_use_vector_kernel) {
             if (!ggml_is_quantized(K->type) && !ggml_is_quantized(V->type)) {
-                if (Q->ne[1] == 1 && Q->ne[3] == 1 && !(gqa_ratio > 4 && K->ne[1] >= 8192)) {
+                if (Q->ne[1] <= 2) {
                     return BEST_FATTN_KERNEL_VEC;
                 }
             } else {
