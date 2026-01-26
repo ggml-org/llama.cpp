a8a8e9261f234fa7296cd2eebb472da8de8fe8c4
Alberto Cabrera <alberto.cabrera@liquid.ai>
2026-01-26 13:38:13 +0000

    wip: GEMM implementation

--- ggml/src/ggml-cpu/arch-fallback.h
+++ ggml/src/ggml-cpu/arch-fallback.h
@@ -72,7 +72,6 @@
 #define ggml_gemv_q2_K_8x8_q8_K_generic ggml_gemv_q2_K_8x8_q8_K
 #define ggml_gemm_iq4_nl_8x8_q8_0_generic ggml_gemm_iq4_nl_8x8_q8_0
 #define ggml_gemm_q2_K_8x8_q8_K_generic ggml_gemm_q2_K_8x8_q8_K
-#define ggml_gemm_q5_K_8x4_q8_K_generic ggml_gemm_q5_K_8x4_q8_K
 #elif defined(__x86_64__) || defined(__i386__) || defined(_M_IX86) || defined(_M_X64)
 // repack.cpp
 #define ggml_quantize_mat_q8_0_4x4_generic ggml_quantize_mat_q8_0_4x4
