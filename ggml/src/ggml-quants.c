e7d9fdddac3cdf2e9aef25a8dfb7e6d4059f0b6e
Charles Xu <charles.xu@arm.com>
2026-02-07 12:33:26 +0100

    fix ci build warnings

--- ggml/src/ggml-quants.c
+++ ggml/src/ggml-quants.c
@@ -2357,8 +2357,7 @@ static inline uint8_t map_int8_to_uint2_idx(int32_t v0) {
         case 3:
         return 3;
         default:
-        GGML_ASSERT(false);
-        return 0;
+        GGML_UNREACHABLE();
     }
 }
 
@@ -2374,8 +2373,7 @@ static inline int32_t map_uint2_idx_to_int8(uint8_t v0) {
         case 3:
         return 3;
         default:
-        GGML_ASSERT(false);
-        return 0;
+        GGML_UNREACHABLE();
     }
 }
 
