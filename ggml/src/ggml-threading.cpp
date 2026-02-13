2ef5856e0addbb655da4b5c3d8c57894cdf9e99b
German Semenov <GermanAizek@yandex.ru>
2026-02-13 21:00:53 +0300

    ggml-threading: use transactional memory from TSX instructions
    This has become relevant again, as Linux since version 7.0 makes unlocking TSX instructions automatic.
    
    References:
    - https://en.wikipedia.org/wiki/Transactional_Synchronization_Extensions
    - https://www.phoronix.com/news/Intel-TSX-Auto-Linux-7.0
    - https://natsys-lab.blogspot.com/2013/11/studying-intel-tsx-performance.html
    
    Signed-off-by: Herman Semenoff <GermanAizek@yandex.ru>

--- ggml/src/ggml-threading.cpp
+++ ggml/src/ggml-threading.cpp
@@ -1,12 +1,27 @@
 #include "ggml-threading.h"
 #include <mutex>
 
+#if defined(__x86_64__) || defined(_M_X64)
+#include <immintrin.h>
+#endif
+
 std::mutex ggml_critical_section_mutex;
 
 void ggml_critical_section_start() {
+#if defined(__x86_64__) || defined(_M_X64)
+    if (_xbegin() == _XBEGIN_STARTED) {
+        return;
+    }
+#endif
     ggml_critical_section_mutex.lock();
 }
 
 void ggml_critical_section_end(void) {
+#if defined(__x86_64__) || defined(_M_X64)
+    if (_xtest()) {
+        _xend();
+        return;
+    }
+#endif
     ggml_critical_section_mutex.unlock();
 }
