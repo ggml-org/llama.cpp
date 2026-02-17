6c037bc112f92bf873841dccf236445d8dd88840
Manogna-Sree <elisetti.manognasree@multicorewareinc.com>
2026-02-17 23:59:36 +0530

    Clean up of the code

--- ggml/src/ggml-cpu/repack.cpp
+++ ggml/src/ggml-cpu/repack.cpp
@@ -894,7 +894,6 @@ void ggml_gemv_q5_K_8x8_q8_K_generic(int                        n,
         }
         for (int j = 0; j < ncols_interleaved; j++) {
             s[x * ncols_interleaved + j] = sumf[j] - sum_minf[j];
-            int index = x * ncols_interleaved + j;
 
         }
     }
