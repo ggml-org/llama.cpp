4bfb1da73aec3a0ffbdb9e5b00d53b793e8099ef
matrixportalx <matrixportalx@proton.me>
2026-02-19 16:15:45 +0000

    Update MainActivity.kt

--- examples/llama.android/app/src/main/java/com/example/llama/MainActivity.kt
+++ examples/llama.android/app/src/main/java/com/example/llama/MainActivity.kt
@@ -55,7 +55,9 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         onBackPressedDispatcher.addCallback { Log.w(TAG, "Ignore back press") }
-
+        
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
         statusTv = findViewById(R.id.gguf)
         messagesRv = findViewById(R.id.messages)
         messagesRv.layoutManager = LinearLayoutManager(this).apply { stackFromEnd = true }
