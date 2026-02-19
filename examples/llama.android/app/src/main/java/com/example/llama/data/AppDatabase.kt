e4ff8382b9e8f1d2394bcfb8108e4f294b084692
matrixportalx <matrixportalx@proton.me>
2026-02-19 22:38:47 +0000

    Update AppDatabase.kt

--- examples/llama.android/app/src/main/java/com/example/llama/data/AppDatabase.kt
+++ examples/llama.android/app/src/main/java/com/example/llama/data/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Conversation::class, Message::class], version = 1, exportSchema = false)
+@Database(entities = [Conversation::class, DbMessage::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun chatDao(): ChatDao
 
