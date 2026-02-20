395f96eae52c6922dfb4d5ed9a908153e0677c79
matrixportalx <matrixportalx@proton.me>
2026-02-20 00:23:57 +0000

    Update build.gradle.kts

--- examples/llama.android/build.gradle.kts
+++ examples/llama.android/build.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("com.google.devtools.ksp") version "2.3.0-1.0.31" apply false
+    id("com.google.devtools.ksp") version "2.1.21-1.0.31" apply false
 }
