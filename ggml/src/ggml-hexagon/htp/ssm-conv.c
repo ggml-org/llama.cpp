e0e10d7908645f81a9748185a8f5090a1d8d8004
Todor Boinovski <todorb@qti.qualcomm.com>
2026-02-20 12:29:00 -0800

    hexagon: added dma to ssm-conv hvx kernel

--- ggml/src/ggml-hexagon/htp/ssm-conv.c
+++ ggml/src/ggml-hexagon/htp/ssm-conv.c
@@ -20,9 +20,7 @@
 #include "htp-ops.h"
 #include "hvx-utils.h"
 
-#include "hvx-dump.h"
-
-#define htp_ssm_conv_tensors_preamble    \
+#define htp_ssm_conv_tensors_preamble                        \
     struct htp_tensor * restrict src0    = &octx->src0;      \
     struct htp_tensor * restrict src1    = &octx->src1;      \
     struct htp_tensor * restrict dst     = &octx->dst;       \
@@ -30,46 +28,46 @@
     struct htp_spad * restrict src1_spad = &octx->src1_spad; \
     struct htp_spad * restrict dst_spad  = &octx->dst_spad;  \
                                                              \
-    const uint32_t ne00 = src0->ne[0]; \
-    const uint32_t ne01 = src0->ne[1]; \
-    const uint32_t ne02 = src0->ne[2]; \
-    const uint32_t ne03 = src0->ne[3]; \
-                                       \
-    const uint32_t ne10 = src1->ne[0]; \
-    const uint32_t ne11 = src1->ne[1]; \
-    const uint32_t ne12 = src1->ne[2]; \
-    const uint32_t ne13 = src1->ne[3]; \
-                                       \
-    const uint32_t ne0 = dst->ne[0];   \
-    const uint32_t ne1 = dst->ne[1];   \
-    const uint32_t ne2 = dst->ne[2];   \
-    const uint32_t ne3 = dst->ne[3];   \
-                                       \
-    const uint32_t nb00 = src0->nb[0]; \
-    const uint32_t nb01 = src0->nb[1]; \
-    const uint32_t nb02 = src0->nb[2]; \
-    const uint32_t nb03 = src0->nb[3]; \
-                                       \
-    const uint32_t nb10 = src1->nb[0]; \
-    const uint32_t nb11 = src1->nb[1]; \
-    const uint32_t nb12 = src1->nb[2]; \
-    const uint32_t nb13 = src1->nb[3]; \
-                                       \
-    const uint32_t nb0 = dst->nb[0];   \
-    const uint32_t nb1 = dst->nb[1];   \
-    const uint32_t nb2 = dst->nb[2];   \
+    const uint32_t ne00 = src0->ne[0];                       \
+    const uint32_t ne01 = src0->ne[1];                       \
+    const uint32_t ne02 = src0->ne[2];                       \
+    const uint32_t ne03 = src0->ne[3];                       \
+                                                             \
+    const uint32_t ne10 = src1->ne[0];                       \
+    const uint32_t ne11 = src1->ne[1];                       \
+    const uint32_t ne12 = src1->ne[2];                       \
+    const uint32_t ne13 = src1->ne[3];                       \
+                                                             \
+    const uint32_t ne0 = dst->ne[0];                         \
+    const uint32_t ne1 = dst->ne[1];                         \
+    const uint32_t ne2 = dst->ne[2];                         \
+    const uint32_t ne3 = dst->ne[3];                         \
+                                                             \
+    const uint32_t nb00 = src0->nb[0];                       \
+    const uint32_t nb01 = src0->nb[1];                       \
+    const uint32_t nb02 = src0->nb[2];                       \
+    const uint32_t nb03 = src0->nb[3];                       \
+                                                             \
+    const uint32_t nb10 = src1->nb[0];                       \
+    const uint32_t nb11 = src1->nb[1];                       \
+    const uint32_t nb12 = src1->nb[2];                       \
+    const uint32_t nb13 = src1->nb[3];                       \
+                                                             \
+    const uint32_t nb0 = dst->nb[0];                         \
+    const uint32_t nb1 = dst->nb[1];                         \
+    const uint32_t nb2 = dst->nb[2];                         \
     const uint32_t nb3 = dst->nb[3];
 
-#define htp_ssm_conv_preamble            \
-    htp_ssm_conv_tensors_preamble;       \
-    dma_queue *dma_queue           = octx->ctx->dma[ith];         \
-    uint32_t src0_nrows_per_thread = octx->src0_nrows_per_thread;
+#define htp_ssm_conv_preamble                                \
+    htp_ssm_conv_tensors_preamble;                           \
+    dma_queue * dma_queue             = octx->ctx->dma[ith]; \
+    uint32_t    src0_nrows_per_thread = octx->src0_nrows_per_thread;
+
+#define SSM_CONV_GATHER_SPAD_SIZE 2048
 
 // Scalar FP32 SSM_CONV implementation
 static void ssm_conv_thread_f32_f32(struct htp_ops_context * octx, uint32_t nth, uint32_t ith) {
-    const struct htp_tensor * src0 = &octx->src0;  // conv_x input   -> {d_conv - 1 + n_t, d_inner, n_seqs}
-    const struct htp_tensor * src1 = &octx->src1;  // conv1d weights -> {d_conv, d_inner}
-    struct htp_tensor *       dst  = &octx->dst;   // output -> {d_inner, n_t, n_seqs}
+    htp_ssm_conv_tensors_preamble;
 
     uint64_t t1, t2;
     t1 = HAP_perf_get_qtimer_count();
@@ -168,6 +166,7 @@ static void ssm_conv_thread_f32_f32_hvx(struct htp_ops_context * octx, uint32_t 
     }
     uint32_t src1_gather_len = VLEN * d_conv;
 
+    // gather scratchpads
     HVX_Vector * src0_vec = (HVX_Vector *) (octx->ctx->vtcm_base + ith * VLEN);
     HVX_Vector * src1_vec = (HVX_Vector *) (octx->ctx->vtcm_base + 1024 + ith * VLEN);
 
@@ -177,14 +176,20 @@ static void ssm_conv_thread_f32_f32_hvx(struct htp_ops_context * octx, uint32_t 
     uint8_t * spad_src0 = octx->src0_spad.data + ith * octx->src0_spad.size_per_thread;
     uint8_t * spad_src1 = octx->src1_spad.data + ith * octx->src1_spad.size_per_thread;
 
-    memcpy(spad_src1, data_src1, octx->src1_spad.size_per_thread);
+    // copy src1 workload to VTCM
+    dma_queue_push_ddr_to_vtcm(dma_queue, dma_make_ptr(spad_src1, data_src1), nb11, nb11, ir);
 
     for (uint32_t i3 = 0; i3 < n_s; ++i3) {
         float * src0_data_ptr = (float *) ((char *) data_src0 + i3 * (src0->nb[2]));
 
-        memcpy(spad_src0, src0_data_ptr, octx->src0_spad.size_per_thread);
+        // copy src0 workload to VTCM
+        dma_queue_push_ddr_to_vtcm(dma_queue, dma_make_ptr(spad_src0, src0_data_ptr), nb01, nb01, ir);
+        dma_queue_flush(dma_queue);
 
         for (uint32_t i2 = 0; i2 < n_t; ++i2) {
+            float * dst_ptr =
+                (float *) ((char *) dst->data + ir0 * (dst->nb[0]) + i2 * (dst->nb[1]) + i3 * (dst->nb[2]));
+
             for (uint32_t i1 = 0; i1 < ir; i1 += VLEN_FP32) {
                 HVX_Vector acc_vec = Q6_V_vzero();
 
@@ -192,17 +197,15 @@ static void ssm_conv_thread_f32_f32_hvx(struct htp_ops_context * octx, uint32_t 
                     Q6_vgather_ARMVw(src0_vec,
                                      SCATTER_TYPE(spad_src0 + (i0 + i1 * ncs) * sizeof(float) + i2 * (src0->nb[0])),
                                      src0_gather_len, (*(const HVX_Vector *) src0_offsets));
-                    Q6_vgather_ARMVw(src1_vec,
-                                     SCATTER_TYPE(spad_src1 + (i0 + i1 * nc) * sizeof(float)),
+                    Q6_vgather_ARMVw(src1_vec, SCATTER_TYPE(spad_src1 + (i0 + i1 * nc) * sizeof(float)),
                                      src1_gather_len, (*(const HVX_Vector *) src1_offsets));
 
                     HVX_Vector prod = Q6_Vqf32_vmpy_VsfVsf(*(const HVX_Vector *) src0_vec, *(const HVX_Vector *) src1_vec);
-                    acc_vec         = Q6_Vqf32_vadd_Vqf32Vqf32(acc_vec, prod);
+                    acc_vec = Q6_Vqf32_vadd_Vqf32Vqf32(acc_vec, prod);
                 }
 
-                float * dst_ptr = (float *) ((char *) dst->data  + i1*sizeof(float) + ir0*(dst->nb[0]) + i2*(dst->nb[1]) + i3*(dst->nb[2]));
-                HVX_Vector result_vec   = Q6_Vsf_equals_Vqf32(acc_vec);
-                *(HVX_Vector *) dst_ptr = result_vec;
+                HVX_Vector result_vec          = Q6_Vsf_equals_Vqf32(acc_vec);
+                *(HVX_Vector *) (dst_ptr + i1) = result_vec;
             }
         }
     }
@@ -228,8 +231,6 @@ static void ssm_conv_work_f32_f32_hvx(unsigned int nth, unsigned int ith, void *
 int op_ssm_conv_f32(struct htp_ops_context * octx) {
     htp_ssm_conv_tensors_preamble;
 
-    assert(sizeof(float) == SIZEOF_FP32);
-
     if (src0->type != HTP_TYPE_F32 || src1->type != HTP_TYPE_F32 || dst->type != HTP_TYPE_F32) {
         FARF(ERROR, "ssm_conv: only (F32 x F32 -> F32) OPs supported");
         return HTP_STATUS_NO_SUPPORT;
@@ -243,40 +244,49 @@ int op_ssm_conv_f32(struct htp_ops_context * octx) {
     if (!(octx->flags & HTP_OPFLAGS_SKIP_COMPUTE)) {
         const uint32_t n_jobs = MIN(octx->n_threads, d_inner);
 
-        int use_hvx = 0;
+        uint32_t use_hvx = 0;
         if (d_inner >= VLEN_FP32 && d_inner % VLEN_FP32 == 0) {
             int is_aligned = hex_is_aligned((void *) src0->data, VLEN) &&
                              hex_is_aligned((void *) src1->data, VLEN) &&
                              hex_is_aligned((void *) dst->data, VLEN);
 
-            if (is_aligned && n_t > 3) {
+            if (is_aligned) {
                 use_hvx = 1;
             }
         }
 
-        FARF(HIGH, "ssm-conv-f32: (%ux%ux%ux%u) x (%ux%ux%ux%u) -> (%ux%ux%ux%u) : use_hvx %d\n",
-                src0->ne[0], src0->ne[1], src0->ne[2], src0->ne[3], src1->ne[0], src1->ne[1], src1->ne[2], src1->ne[3],
-                dst->ne[0], dst->ne[1], dst->ne[2], dst->ne[3], use_hvx);
-
-        // chunks per thread
+        // d_inner chunks per thread
         const int dr = (d_inner + n_jobs - 1) / n_jobs;
 
-        octx->dst_spad.size_per_thread  = hex_round_up(dr * ne1 * ne2 * ne3 * sizeof(float), 256);
-        octx->src0_spad.size_per_thread = hex_round_up(ne00 * dr * sizeof(float), 256);
-        octx->src1_spad.size_per_thread = hex_round_up(ne10 * dr * sizeof(float), 256);
+        octx->src0_spad.size_per_thread = hex_round_up(dr * nb01, 256);
+        octx->src1_spad.size_per_thread = hex_round_up(dr * nb11, 256);
+        octx->dst_spad.size_per_thread  = hex_round_up(dr * sizeof(float), 256);
 
         octx->src1_spad.size = octx->src1_spad.size_per_thread * octx->n_threads;
         octx->src0_spad.size = octx->src0_spad.size_per_thread * octx->n_threads;
         octx->dst_spad.size  = octx->dst_spad.size_per_thread * octx->n_threads;
 
-        octx->src0_spad.data = octx->ctx->vtcm_base + 2048;
+        octx->src0_spad.data = octx->ctx->vtcm_base + SSM_CONV_GATHER_SPAD_SIZE;
         octx->src1_spad.data = octx->src0_spad.data + octx->src0_spad.size;
         octx->dst_spad.data  = octx->src1_spad.data + octx->src1_spad.size;
 
         FARF(HIGH, "ssm_conv-f32: spad-per-thread:(%u:%u:%u) spad-sizes:(%u:%u:%u) spad-data:(%p:%p:%p)\n",
-             octx->src0_spad.size_per_thread, octx->src1_spad.size_per_thread, octx->dst_spad.size_per_thread,
-             octx->src0_spad.size, octx->src1_spad.size, octx->dst_spad.size, octx->src0_spad.data,
-             octx->src1_spad.data, octx->dst_spad.data);
+                octx->src0_spad.size_per_thread, octx->src1_spad.size_per_thread, octx->dst_spad.size_per_thread,
+                octx->src0_spad.size, octx->src1_spad.size, octx->dst_spad.size, octx->src0_spad.data,
+                octx->src1_spad.data, octx->dst_spad.data);
+
+        const size_t total_spad_size =
+            SSM_CONV_GATHER_SPAD_SIZE + octx->src0_spad.size + octx->src1_spad.size + octx->dst_spad.size;
+
+        if (total_spad_size > octx->ctx->vtcm_size) {
+            FARF(HIGH, "ssm_conv-f32: HVX scratchpad size %zu exceeds VTCM size %zu",
+                 total_spad_size, octx->ctx->vtcm_size);
+            use_hvx = 0;
+        }
+
+        FARF(HIGH, "ssm-conv-f32: (%ux%ux%ux%u) x (%ux%ux%ux%u) -> (%ux%ux%ux%u) : use_hvx %d\n", src0->ne[0],
+             src0->ne[1], src0->ne[2], src0->ne[3], src1->ne[0], src1->ne[1], src1->ne[2], src1->ne[3], dst->ne[0],
+             dst->ne[1], dst->ne[2], dst->ne[3], use_hvx);
 
         if (use_hvx) {
             worker_pool_run_func(octx->ctx->worker_pool, ssm_conv_work_f32_f32_hvx, octx, n_jobs);
