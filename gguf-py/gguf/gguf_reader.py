12c719b3f12fe55042669291e88da2d02ad20c70
Georgi Gerganov <ggerganov@gmail.com>
2026-02-22 09:47:43 +0200

    gguf-py : error on duplicate keys when reading

--- gguf-py/gguf/gguf_reader.py
+++ gguf-py/gguf/gguf_reader.py
@@ -203,11 +203,11 @@ class GGUFReader:
 
     def _push_field(self, field: ReaderField, skip_sum: bool = False) -> int:
         if field.name in self.fields:
-            # TODO: add option to generate error on duplicate keys
-            # raise KeyError(f'Duplicate {field.name} already in list at offset {field.offset}')
+            # TODO: add option to make this a warning and accept duplicate keys like below
+            raise KeyError(f'Duplicate {field.name} already in list at offset {field.offset}')
 
-            logger.warning(f'Duplicate key {field.name} at offset {field.offset}')
-            self.fields[field.name + '_{}'.format(field.offset)] = field
+            #logger.warning(f'Duplicate key {field.name} at offset {field.offset}')
+            #self.fields[field.name + '_{}'.format(field.offset)] = field
         else:
             self.fields[field.name] = field
         return 0 if skip_sum else sum(int(part.nbytes) for part in field.parts)
