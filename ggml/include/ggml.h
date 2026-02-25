b0c75e227ad7b9bd3c83e2df46167a87e69cf2ac
richarddd <richard.davison1@gmail.com>
2026-02-25 20:15:25 +0100

    correct comment

--- ggml/include/ggml.h
+++ ggml/include/ggml.h
@@ -427,7 +427,7 @@ extern "C" {
         // GGML_TYPE_IQ4_NL_4_8 = 37,
         // GGML_TYPE_IQ4_NL_8_8 = 38,
         GGML_TYPE_MXFP4   = 39, // MXFP4 (1 block)
-        GGML_TYPE_NVFP4   = 40, // NVFP4 (1 block, E4M3 scale)
+        GGML_TYPE_NVFP4   = 40, // NVFP4 (4 blocks, E4M3 scale)
         GGML_TYPE_COUNT   = 41,
     };
 
