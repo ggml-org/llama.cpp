90df9bce8d23ecaa202a4684442d724a83fc5308
Rashidul Islam <rasidulislam71@gmail.com>
2026-02-26 16:49:42 +0530

    metal:add conv_3d backend

--- ggml/src/ggml-metal/ggml-metal-impl.h
+++ ggml/src/ggml-metal/ggml-metal-impl.h
@@ -641,6 +641,42 @@ typedef struct {
     int32_t  KHW; // KH * KW, pre-computed on CPU to save GPU resources
 } ggml_metal_kargs_im2col;
 
+typedef struct {
+    int32_t  IW;
+    int32_t  IH;
+    int32_t  ID;
+    int32_t  OW;
+    int32_t  OH;
+    int32_t  OD;
+    int32_t  KW;
+    int32_t  KH;
+    int32_t  KD;
+    int32_t  s0;
+    int32_t  s1;
+    int32_t  s2;
+    int32_t  p0;
+    int32_t  p1;
+    int32_t  p2;
+    int32_t  d0;
+    int32_t  d1;
+    int32_t  d2;
+    int32_t  IC;
+    int32_t  N;
+    int32_t  OC;
+    uint64_t nb0_0;
+    uint64_t nb0_1;
+    uint64_t nb0_2;
+    uint64_t nb0_3;
+    uint64_t nb1_0;
+    uint64_t nb1_1;
+    uint64_t nb1_2;
+    uint64_t nb1_3;
+    uint64_t nb_0;
+    uint64_t nb_1;
+    uint64_t nb_2;
+    uint64_t nb_3;
+} ggml_metal_kargs_conv_3d;
+
 typedef struct{
     int32_t  ne00;
     uint64_t nb01;
