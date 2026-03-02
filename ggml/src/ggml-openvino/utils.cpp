d25211e337b2d8ca993baee8581a0db5ebda4e27
Yu, Zijun <zijun.yu@intel.com>
2026-03-02 11:38:29 +0800

    Fix test-backend-ops for OV GPU LNL

--- ggml/src/ggml-openvino/utils.cpp
+++ ggml/src/ggml-openvino/utils.cpp
@@ -468,7 +468,11 @@ enum ggml_status naive_compute(ggml_cgraph * cgraph,
 
     ov::InferRequest infer_request;
     auto remote_context = ggml_openvino_get_remote_context();
-    core.set_property(device, ov::hint::execution_mode(ov::hint::ExecutionMode::ACCURACY));
+    if (cgraph->nodes[0]->op == GGML_OP_MUL_MAT) {
+        core.set_property(device, ov::hint::execution_mode(ov::hint::ExecutionMode::PERFORMANCE));
+    } else {
+        core.set_property(device, ov::hint::execution_mode(ov::hint::ExecutionMode::ACCURACY));
+    }
     if (remote_context.has_value()) {
         infer_request = core.compile_model(model, remote_context.value(), config).create_infer_request();
     } else {
