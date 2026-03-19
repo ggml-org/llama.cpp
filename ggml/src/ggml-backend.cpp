a48fd3bedc624f2091f525c3658fef8847953324
aendk <akieslinger@nvidia.com>
2026-03-19 14:54:34 +0100

    Fix: For pipeline parallelism, adds missing synchronization between input copying and graph computation.

--- ggml/src/ggml-backend.cpp
+++ ggml/src/ggml-backend.cpp
@@ -1578,9 +1578,7 @@ static enum ggml_status ggml_backend_sched_compute_splits(ggml_backend_sched_t s
             }
         }
 
-        if (sched->events[split_backend_id][sched->cur_copy] == NULL) {
-            ggml_backend_synchronize(split_backend);
-        }
+        ggml_backend_synchronize(split_backend);
 
         if (!sched->callback_eval) {
             enum ggml_status ec = ggml_backend_graph_compute_async(split_backend, &split->graph);
