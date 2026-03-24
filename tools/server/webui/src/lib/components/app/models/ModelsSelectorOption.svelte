b94f1d63d60ba445abb1d25f214e600e25a518bc
Pascal <admin@serveurperso.com>
2026-03-24 14:16:38 +0100

    webui: restore orange sleeping indicator dot with hover unload

--- tools/server/webui/src/lib/components/app/models/ModelsSelectorOption.svelte
+++ tools/server/webui/src/lib/components/app/models/ModelsSelectorOption.svelte
@@ -134,16 +134,19 @@
 			</div>
 		{:else if isSleeping}
 			<div class="flex w-4 items-center justify-center">
-				<ActionIcon
-					iconSize="h-2.5 w-2.5"
-					icon={PowerOff}
-					tooltip="Unload model"
-					class="h-3 w-3 text-red-500 hover:text-red-600"
-					onclick={(e) => {
-						e?.stopPropagation();
-						modelsStore.unloadModel(option.model);
-					}}
-				/>
+				<span class="h-2 w-2 rounded-full bg-orange-400 group-hover:hidden"></span>
+
+				<!-- svelte-ignore a11y_no_static_element_interactions -->
+				<!-- svelte-ignore a11y_click_events_have_key_events -->
+				<div class="hidden group-hover:flex" onclick={(e) => e.stopPropagation()}>
+					<ActionIcon
+						iconSize="h-2.5 w-2.5"
+						icon={PowerOff}
+						tooltip="Unload model"
+						class="h-3 w-3 text-red-500 hover:text-red-600"
+						onclick={() => modelsStore.unloadModel(option.model)}
+					/>
+				</div>
 			</div>
 		{:else if isLoaded}
 			<div class="flex w-4 items-center justify-center">
