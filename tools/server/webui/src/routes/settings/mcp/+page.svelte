7a13b4191a05fce7a34051e2e560fd8a7fb11b3c
Aleksander Grygier <aleksander.grygier@gmail.com>
2026-04-01 13:01:48 +0200

    feat: WIP

--- tools/server/webui/src/routes/settings/mcp/+page.svelte
+++ tools/server/webui/src/routes/settings/mcp/+page.svelte
@@ -2,4 +2,4 @@
 	import { McpServersSettings } from '$lib/components/app';
 </script>
 
-<McpServersSettings class="mx-auto w-full p-4 md:p-8 md:pt-0!" />
+<McpServersSettings class="mx-auto w-full p-4 md:p-8" />
