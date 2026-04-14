3ea6b59d7b9b79df0f390e5767ee92d2bf8c8dfd
Michael Wand <michael@baybridgeaquarium.com>
2026-04-14 16:03:41 -0700

    Removed whitespace from comment

--- ggml/src/ggml-cuda/common.cuh
+++ ggml/src/ggml-cuda/common.cuh
@@ -843,7 +843,7 @@ static __device__ __forceinline__ uint8_t ggml_cuda_fp32_to_ue4m3(float x) {
     const __nv_fp8_e4m3 xf(x);
     return xf.__x;
 #else
-     NO_DEVICE_CODE; // Used only for NVFP4 Scales for Activations, only for Blackwell 
+     NO_DEVICE_CODE; // Used only for NVFP4 Scales for Activations, only for Blackwell
 #endif // defined(BLACKWELL_MMA_AVAILABLE)
 }
 
