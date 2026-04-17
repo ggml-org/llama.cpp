553c3a85c2e025f30b7ffcdd442d7f2f24047088
Michael Wand <michael@baybridgeaquarium.com>
2026-04-17 13:39:15 -0700

    Update ggml/src/ggml-cuda/mmq.cuh
    Co-authored-by: Johannes Gäßler <johannesg@5d6.de>

--- ggml/src/ggml-cuda/mmq.cuh
+++ ggml/src/ggml-cuda/mmq.cuh
@@ -46,7 +46,7 @@ struct block_q8_1_mmq {
     int8_t qs[4*QK8_1]; // 128 values quantized to 8 bit each
 };
 
-// this struct is used for fp4 data types (currently only used in Blackwell)
+// this struct is used for fp4 data types (currently only used for Blackwell)
 // mxfp4 has block size 32, each int32 of d4 contains 2 e8m0 scales in the lower 16 bits
 // nvfp4 has block size 16, each int32 of d4 contains 4 ue4m3 scales
 struct block_fp4_mmq {
