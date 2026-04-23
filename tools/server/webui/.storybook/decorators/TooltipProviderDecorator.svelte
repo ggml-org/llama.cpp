73d6121b747fe83664a0375ea6ae1e50e187fc21
Aleksander Grygier <aleksander.grygier@gmail.com>
2026-04-23 17:06:30 +0200

    refactor: Storybook cleanup

--- tools/server/webui/.storybook/decorators/TooltipProviderDecorator.svelte
+++ tools/server/webui/.storybook/decorators/TooltipProviderDecorator.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import * as Tooltip from '../src/lib/components/ui/tooltip';
+	import * as Tooltip from '../../src/lib/components/ui/tooltip';
 
 	interface Props {
 		children: any;
