f84632951a4c15554ee387f6007951edde8f3c75
Georgi Gerganov <ggerganov@gmail.com>
2026-04-25 18:27:15 +0300

    wip

--- src/models/eagle3.cpp
+++ src/models/eagle3.cpp
@@ -15,9 +15,9 @@ void llama_model_eagle3::load_arch_hparams(llama_model_loader & ml) {
             hparams.eagle3_extract_layers[2]);
 
     // EAGLE3 target model hidden size
-    ml.get_key(LLM_KV_EAGLE3_TARGET_HIDDEN_SIZE, hparams.eagle3_target_hidden_size);
+    ml.get_key(LLM_KV_EAGLE3_TARGET_HIDDEN_SIZE, hparams.n_embd_inp_impl);
     LLAMA_LOG_INFO("%s: EAGLE3 target_hidden_size = %u (draft n_embd = %u)\n", __func__,
-            hparams.eagle3_target_hidden_size, hparams.n_embd);
+            hparams.n_embd_inp_impl, hparams.n_embd);
 
     // EAGLE3 norm_before_residual (optional, default false)
     // compatible with Readhat eagle3 speculator model
@@ -29,10 +29,10 @@ void llama_model_eagle3::load_arch_hparams(llama_model_loader & ml) {
     type = LLM_TYPE_UNKNOWN;
 }
 
-void llama_model_eagle3::load_arch_tensors(llama_model_loader &) {
+void llama_model_eagle3::load_arch_tensors(llama_model_loader & /*ml*/) {
     LLAMA_LOAD_LOCALS;
 
-    const int64_t n_embd_target_features = 3 * hparams.eagle3_target_hidden_size;
+    const int64_t n_embd_inp = hparams.n_embd_inp();
     const int64_t n_embd_attn_input = 2 * n_embd;
 
     // Get vocab size from the d2t tensor in the GGUF file (optional - only needed if EAGLE3 has different vocab_size than target)
@@ -49,7 +49,7 @@ void llama_model_eagle3::load_arch_tensors(llama_model_loader &) {
     }
 
     // Feature fusion layer: projects 3 target layers to draft hidden size
-    fc = create_tensor(tn(LLM_TENSOR_EAGLE3_FC, "weight"), {n_embd_target_features, n_embd}, 0);
+    fc = create_tensor(tn(LLM_TENSOR_EAGLE3_FC, "weight"), {n_embd_inp, n_embd}, 0);
 
     // Output layer (uses draft vocab size)
     output_norm = create_tensor(tn(LLM_TENSOR_OUTPUT_NORM, "weight"), {n_embd}, 0);
@@ -103,14 +103,14 @@ std::unique_ptr<llm_graph_context> llama_model_eagle3::build_arch_graph(const ll
 
 template <>
 ggml_tensor * llama_model_eagle3::graph<true>::build_inp_embd_enc() const {
-    const int64_t n_embd_target_features = 3 * hparams.eagle3_target_hidden_size;
+    const int64_t n_embd_inp = hparams.n_embd_inp();
 
     ggml_tensor * cur = nullptr;
 
     // Input: Target model features (3 layers concatenated: low, mid, high)
     // Data will be provided via ubatch->embd in encode_eagle3_features()
-    auto inp_target = std::make_unique<llm_graph_input_embd>(n_embd_target_features);
-    inp_target->embd = ggml_new_tensor_2d(ctx0, GGML_TYPE_F32, n_embd_target_features, n_tokens);
+    auto inp_target = std::make_unique<llm_graph_input_embd>(n_embd_inp);
+    inp_target->embd = ggml_new_tensor_2d(ctx0, GGML_TYPE_F32, n_embd_inp, n_tokens);
     ggml_set_input(inp_target->embd);
 
     cur = inp_target->embd;
@@ -129,6 +129,9 @@ llama_model_eagle3::graph<true>::graph(const llama_model & model, const llm_grap
     ggml_tensor * cur = nullptr;
 
     cur = build_inp_embd_enc();
+
+    // sanity check
+    GGML_ASSERT(hparams.n_embd_inp() == model.fc->ne[0]);
 
     // Feature fusion layer
     cur = build_lora_mm(model.fc, cur);
