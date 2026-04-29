06e796b016d974932b9807b50531f80dfae22a84
Reese Levine <reeselevine1@gmail.com>
2026-04-29 07:47:09 -0700

    Update to gguf.cpp style

--- src/llama-mmap.cpp
+++ src/llama-mmap.cpp
@@ -40,6 +40,14 @@
 #include <TargetConditionals.h>
 #endif
 
+#ifdef _WIN32
+#    define llama_mmap_ftell _ftelli64
+#    define llama_mmap_fseek _fseeki64
+#else
+#    define llama_mmap_ftell ftello
+#    define llama_mmap_fseek fseeko
+#endif
+
 // TODO: consider moving to llama-impl.h if needed in more places
 #if defined(_WIN32)
 static std::string llama_format_win_err(DWORD err) {
@@ -226,12 +234,7 @@ struct llama_file::impl {
 
     size_t tell() const {
         if (fd == -1) {
-#ifdef __EMSCRIPTEN__
-            // in 32-bit WebAssembly, ftello (64-bit signed return value) is necessary to support files larger than 2GB
-            off_t ret = ftello(fp);
-#else
-            long ret = std::ftell(fp);
-#endif
+            off_t ret = llama_mmap_ftell(fp);
             if (ret == -1) {
                 throw std::runtime_error(format("ftell error: %s", strerror(errno)));
             }
@@ -249,12 +252,7 @@ struct llama_file::impl {
     void seek(size_t offset, int whence) const {
         off_t ret = 0;
         if (fd == -1) {
-#ifdef __EMSCRIPTEN__
-            // in 32-bit WebAssembly, fseeko (64-bit signed return value) is necessary to support files larger than 2GB
-            ret = fseeko(fp, (off_t) offset, whence);
-#else
-            ret = std::fseek(fp, (long) offset, whence);
-#endif
+            ret = llama_mmap_fseek(fp, offset, whence);
         } else {
             ret = lseek(fd, offset, whence);
         }
