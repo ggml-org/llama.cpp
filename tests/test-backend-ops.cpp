3718c3a8fabb326526a095337489590460c5cccf
Exile333 <rchabdar@amd.com>
2026-05-08 18:08:35 +0200

    test-backend-ops: add more fields to csv output.
    Currently, csv output at `test-backend-ops` binary doesn't report
    time/tflops/nruns/etc, which makes it not very helpful for automated
    perf evaluation.
    Here I add fields from sql output to csv.

--- tests/test-backend-ops.cpp
+++ tests/test-backend-ops.cpp
@@ -1093,6 +1093,15 @@ struct csv_printer : public printer {
             "test_mode",
             "backend_reg_name",
             "backend_name",
+            "test_time",
+            "build_commit",
+            "passed",
+            "time_us",
+            "flops",
+            "bandwidth_gb_s",
+            "memory_kb",
+            "n_runs",
+            "device_description",
         };
     }
 
