5439ff9a4f36b623b3402870a19de52a4785a756
SaqibAkram-10xE <saqib.akram@10xengineers.ai>
2026-05-11 03:19:05 +0500

    mulmat

--- ggml/src/ggml-et/et-kernels/src/mul_mat_f32.c
+++ ggml/src/ggml-et/et-kernels/src/mul_mat_f32.c
@@ -48,8 +48,8 @@ int entry_point(struct ggml_et_binary_params* params, void* env) {
     float* dst_data       = (float*)dst->data;
 
 #ifdef BUILD_FOR_UBERKERNEL
-    evict_region_past_l2(params->src1.data, tensor_bytes(&params->src1));
-    et_barrier(ET_BARRIER_GLOBAL);
+    // evict_region_past_l2(params->src1.data, tensor_bytes(&params->src1));
+    // et_barrier(ET_BARRIER_GLOBAL);
 #endif
 
     // Dimensions and Strides
