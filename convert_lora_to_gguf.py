0a6805f1605ecb345d9e840be1e23a33fcd44640
Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>
2026-05-11 19:26:20 +0200

    work around ty deficiency

--- convert_lora_to_gguf.py
+++ convert_lora_to_gguf.py
@@ -249,9 +249,9 @@ class LoraTorchTensor:
             else:
                 raise NotImplementedError
         elif func is torch.split:
-            assert len(args) >= 2
-            tensor, split_size = args[0], args[1]  # ty: ignore[index-out-of-bounds]
-            dim = args[2] if len(args) > 2 else kwargs.get("dim", 0)  # ty: ignore[index-out-of-bounds]
+            assert len(args) and len(args) >= 2
+            tensor, split_size = args[0], args[1]
+            dim = args[2] if len(args) > 2 else kwargs.get("dim", 0)
             return tensor.split(split_size, dim=dim)
         else:
             raise NotImplementedError
