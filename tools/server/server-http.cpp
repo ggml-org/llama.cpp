3da31ccbeefe16e303a4308e333c7db5dfc89d78
Aleksander Grygier <aleksander.grygier@gmail.com>
2026-05-12 11:00:25 +0200

    refactor: Logic & security improvements

--- tools/server/server-http.cpp
+++ tools/server/server-http.cpp
@@ -249,7 +249,7 @@ bool server_http_context::init(const common_params & params) {
                 return false;
             }
 #endif
-            // no endpoints is allowed to be accessed when the server is not ready
+            // no endpoints are allowed to be accessed when the server is not ready
             // this is to prevent any data races or inconsistent states
             res.status = 503;
             res.set_content(
