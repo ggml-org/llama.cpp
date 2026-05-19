909bd767c2bb2cc787c7ee86de65f6ded3426c33
Georgi Gerganov <ggerganov@gmail.com>
2026-05-19 14:20:02 +0300

    arg : step-back on adding k4v to the default spec config

--- common/arg.cpp
+++ common/arg.cpp
@@ -4126,10 +4126,11 @@ common_params_context common_params_parser_init(common_params & params, llama_ex
             params.speculative.ngram_mod.n_min = 48;
             params.speculative.ngram_mod.n_max = 64;
 
-            params.speculative.types.push_back(COMMON_SPECULATIVE_TYPE_NGRAM_MAP_K4V);
-            params.speculative.ngram_map_k4v.size_n = 8;
-            params.speculative.ngram_map_k4v.size_m = 24;
-            params.speculative.ngram_map_k4v.min_hits = 2;
+            // TODO: not sure if this is a good config - explore more settings and potentially enable it
+            //params.speculative.types.push_back(COMMON_SPECULATIVE_TYPE_NGRAM_MAP_K4V);
+            //params.speculative.ngram_map_k4v.size_n = 8;
+            //params.speculative.ngram_map_k4v.size_m = 24;
+            //params.speculative.ngram_map_k4v.min_hits = 2;
         }
     ).set_examples({LLAMA_EXAMPLE_SERVER, LLAMA_EXAMPLE_CLI}));
 
