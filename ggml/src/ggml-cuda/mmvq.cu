144e8bb9cead56e79ed9a70b1c0c0a2be0136200
Nguyen Ngoc An <antrc2gamer@gmail.com>
2026-05-22 04:24:14 +0000

    fix cuda kernel

--- ggml/src/ggml-cuda/mmvq.cu
+++ ggml/src/ggml-cuda/mmvq.cu
@@ -29,8 +29,8 @@ static constexpr __device__ vec_dot_q_cuda_t get_vec_dot_q_cuda(ggml_type type) 
         case GGML_TYPE_IQ1_S:   return vec_dot_iq1_s_q8_1;
         case GGML_TYPE_IQ1_M:   return vec_dot_iq1_m_q8_1;
         case GGML_TYPE_IQ4_NL:  return vec_dot_iq4_nl_q8_1;
-        case GGML_TYPE_IQ4_XS:  return VDR_IQ4_XS_Q8_1_MMVQ;
-        case GGML_TYPE_STQ1_0:  return VDR_STQ1_0_Q8_1_MMVQ;  // THÊM DÒNG NÀY
+        case GGML_TYPE_IQ4_XS:  return vec_dot_iq4_xs_q8_1;
+        case GGML_TYPE_STQ1_0:  return vec_dot_stq1_0_q8_1;
         case GGML_TYPE_IQ3_S:   return vec_dot_iq3_s_q8_1;
         default:                return nullptr;
     }
@@ -58,6 +58,7 @@ static constexpr __host__ __device__ int get_vdr_mmvq(ggml_type type) {
         case GGML_TYPE_IQ3_S:   return VDR_IQ3_S_Q8_1_MMVQ;
         case GGML_TYPE_IQ4_NL:  return VDR_IQ4_NL_Q8_1_MMVQ;
         case GGML_TYPE_IQ4_XS:  return VDR_IQ4_XS_Q8_1_MMVQ;
+        case GGML_TYPE_STQ1_0:  return VDR_STQ1_0_Q8_1_MMVQ;  // THÊM
         default:                return 1;
     }
 }
