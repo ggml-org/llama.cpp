9981595801ff973a178fe460b804177d5973efe6
SaqibAkram-10xE <saqib.akram@10xengineers.ai>
2026-05-25 16:29:11 +0500

    bisect2 n64: +mul_mat_Q8_0 only (disable Q4_0)

--- ggml/src/ggml-et/et-kernels/src/uberkernel.c
+++ ggml/src/ggml-et/et-kernels/src/uberkernel.c
@@ -576,11 +576,11 @@ int entry_point(struct ggml_et_uberkernel_params * params, void * env) {
                 rc = mul_mat_Q8_0_entry(p, env);
                 break;
             }
-            case GGML_ET_UBERKERNEL_KERNEL_MUL_MAT_Q4_0: { // gemma4
-                struct ggml_et_binary_params *p = (struct ggml_et_binary_params *) inst_params;
-                rc = mul_mat_Q4_0_entry(p, env);
-                break;
-            }
+            // case GGML_ET_UBERKERNEL_KERNEL_MUL_MAT_Q4_0: { // gemma4
+            //     struct ggml_et_binary_params *p = (struct ggml_et_binary_params *) inst_params;
+            //     rc = mul_mat_Q4_0_entry(p, env);
+            //     break;
+            // }
 
             default:
                 return -1;
