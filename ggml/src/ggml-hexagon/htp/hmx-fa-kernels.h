69fecbc6ae8aa1dadd1e32a492efef0a7ab882af
Max Krasnyansky <maxk@qti.qualcomm.com>
2026-07-01 21:26:34 -0700

    hex-hmx: use inline asm instead of intrinsics to avoid compiler issues

--- ggml/src/ggml-hexagon/htp/hmx-fa-kernels.h
+++ ggml/src/ggml-hexagon/htp/hmx-fa-kernels.h
@@ -53,49 +53,108 @@ static void hmx_fa_qk_dot_tile(
     __fp16 *       out_tile,
     size_t         n_dot_tiles
 ) {
-    asm volatile("" ::: "memory");
     if (n_dot_tiles == 2) {
-        Q6_activation_hf_mxmem_RR((unsigned int) row_tiles, 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) col_tiles, 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (row_tiles + HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (col_tiles + HMX_FP16_TILE_N_ELMS), 2047);
+        asm volatile(
+            "{\n"
+            "    activation.hf = mxmem(%0, %2)\n"
+            "    weight.hf = mxmem(%1, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%3, %2)\n"
+            "    weight.hf = mxmem(%4, %2)\n"
+            "}\n"
+            :
+            : "r"(row_tiles), "r"(col_tiles), "r"(2047),
+              "r"(row_tiles + HMX_FP16_TILE_N_ELMS), "r"(col_tiles + HMX_FP16_TILE_N_ELMS)
+        );
     } else if (n_dot_tiles == 4) {
-        Q6_activation_hf_mxmem_RR((unsigned int) row_tiles, 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) col_tiles, 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (row_tiles + HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (col_tiles + HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (row_tiles + 2 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (col_tiles + 2 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (row_tiles + 3 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (col_tiles + 3 * HMX_FP16_TILE_N_ELMS), 2047);
+        asm volatile(
+            "{\n"
+            "    activation.hf = mxmem(%0, %2)\n"
+            "    weight.hf = mxmem(%1, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%3, %2)\n"
+            "    weight.hf = mxmem(%4, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%5, %2)\n"
+            "    weight.hf = mxmem(%6, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%7, %2)\n"
+            "    weight.hf = mxmem(%8, %2)\n"
+            "}\n"
+            :
+            : "r"(row_tiles), "r"(col_tiles), "r"(2047),
+              "r"(row_tiles + HMX_FP16_TILE_N_ELMS), "r"(col_tiles + HMX_FP16_TILE_N_ELMS),
+              "r"(row_tiles + 2 * HMX_FP16_TILE_N_ELMS), "r"(col_tiles + 2 * HMX_FP16_TILE_N_ELMS),
+              "r"(row_tiles + 3 * HMX_FP16_TILE_N_ELMS), "r"(col_tiles + 3 * HMX_FP16_TILE_N_ELMS)
+        );
     } else if (n_dot_tiles == 8) {
-        Q6_activation_hf_mxmem_RR((unsigned int) row_tiles, 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) col_tiles, 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (row_tiles + HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (col_tiles + HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (row_tiles + 2 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (col_tiles + 2 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (row_tiles + 3 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (col_tiles + 3 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (row_tiles + 4 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (col_tiles + 4 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (row_tiles + 5 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (col_tiles + 5 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (row_tiles + 6 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (col_tiles + 6 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (row_tiles + 7 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (col_tiles + 7 * HMX_FP16_TILE_N_ELMS), 2047);
+        asm volatile(
+            "{\n"
+            "    activation.hf = mxmem(%0, %2)\n"
+            "    weight.hf = mxmem(%1, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%3, %2)\n"
+            "    weight.hf = mxmem(%4, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%5, %2)\n"
+            "    weight.hf = mxmem(%6, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%7, %2)\n"
+            "    weight.hf = mxmem(%8, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%9, %2)\n"
+            "    weight.hf = mxmem(%10, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%11, %2)\n"
+            "    weight.hf = mxmem(%12, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%13, %2)\n"
+            "    weight.hf = mxmem(%14, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%15, %2)\n"
+            "    weight.hf = mxmem(%16, %2)\n"
+            "}\n"
+            :
+            : "r"(row_tiles), "r"(col_tiles), "r"(2047),
+              "r"(row_tiles + HMX_FP16_TILE_N_ELMS), "r"(col_tiles + HMX_FP16_TILE_N_ELMS),
+              "r"(row_tiles + 2 * HMX_FP16_TILE_N_ELMS), "r"(col_tiles + 2 * HMX_FP16_TILE_N_ELMS),
+              "r"(row_tiles + 3 * HMX_FP16_TILE_N_ELMS), "r"(col_tiles + 3 * HMX_FP16_TILE_N_ELMS),
+              "r"(row_tiles + 4 * HMX_FP16_TILE_N_ELMS), "r"(col_tiles + 4 * HMX_FP16_TILE_N_ELMS),
+              "r"(row_tiles + 5 * HMX_FP16_TILE_N_ELMS), "r"(col_tiles + 5 * HMX_FP16_TILE_N_ELMS),
+              "r"(row_tiles + 6 * HMX_FP16_TILE_N_ELMS), "r"(col_tiles + 6 * HMX_FP16_TILE_N_ELMS),
+              "r"(row_tiles + 7 * HMX_FP16_TILE_N_ELMS), "r"(col_tiles + 7 * HMX_FP16_TILE_N_ELMS)
+        );
     } else {
         for (size_t k = 0; k < n_dot_tiles; ++k) {
-            Q6_activation_hf_mxmem_RR((unsigned int) row_tiles, 2047);
-            Q6_weight_hf_mxmem_RR((unsigned int) col_tiles, 2047);
+            asm volatile(
+                "{\n"
+                "    activation.hf = mxmem(%0, %2)\n"
+                "    weight.hf = mxmem(%1, %2)\n"
+                "}\n"
+                :
+                : "r"(row_tiles), "r"(col_tiles), "r"(2047)
+            );
             row_tiles += HMX_FP16_TILE_N_ELMS;
             col_tiles += HMX_FP16_TILE_N_ELMS;
         }
     }
-    asm volatile("" ::: "memory");
-    Q6_mxmem_AR_after_hf(out_tile, 0);
-    asm volatile("" ::: "memory");
+    asm volatile(
+        "mxmem(%0, %1):after.hf = acc\n"
+        :
+        : "r"(out_tile), "r"(0)
+        : "memory"
+    );
 }
 
 static inline void hmx_fa_o_update_tile(
@@ -106,51 +165,116 @@ static inline void hmx_fa_o_update_tile(
     __fp16 *       o_tile_out,
     size_t         n_col_tiles
 ) {
-    asm volatile("" ::: "memory");
-    Q6_activation_hf_mxmem_RR((unsigned int) d_diag, 2047);
-    Q6_weight_hf_mxmem_RR((unsigned int) o_rc, 2047);
+    asm volatile(
+        "{\n"
+        "    activation.hf = mxmem(%0, %2)\n"
+        "    weight.hf = mxmem(%1, %2)\n"
+        "}\n"
+        :
+        : "r"(d_diag), "r"(o_rc), "r"(2047)
+    );
     if (n_col_tiles == 2) {
-        Q6_activation_hf_mxmem_RR((unsigned int) p_tile_in, 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) v_tile_in, 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (p_tile_in + HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (v_tile_in + HMX_FP16_TILE_N_ELMS), 2047);
+        asm volatile(
+            "{\n"
+            "    activation.hf = mxmem(%0, %2)\n"
+            "    weight.hf = mxmem(%1, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%3, %2)\n"
+            "    weight.hf = mxmem(%4, %2)\n"
+            "}\n"
+            :
+            : "r"(p_tile_in), "r"(v_tile_in), "r"(2047),
+              "r"(p_tile_in + HMX_FP16_TILE_N_ELMS), "r"(v_tile_in + HMX_FP16_TILE_N_ELMS)
+        );
     } else if (n_col_tiles == 4) {
-        Q6_activation_hf_mxmem_RR((unsigned int) p_tile_in, 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) v_tile_in, 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (p_tile_in + HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (v_tile_in + HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (p_tile_in + 2 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (v_tile_in + 2 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (p_tile_in + 3 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (v_tile_in + 3 * HMX_FP16_TILE_N_ELMS), 2047);
+        asm volatile(
+            "{\n"
+            "    activation.hf = mxmem(%0, %2)\n"
+            "    weight.hf = mxmem(%1, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%3, %2)\n"
+            "    weight.hf = mxmem(%4, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%5, %2)\n"
+            "    weight.hf = mxmem(%6, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%7, %2)\n"
+            "    weight.hf = mxmem(%8, %2)\n"
+            "}\n"
+            :
+            : "r"(p_tile_in), "r"(v_tile_in), "r"(2047),
+              "r"(p_tile_in + HMX_FP16_TILE_N_ELMS), "r"(v_tile_in + HMX_FP16_TILE_N_ELMS),
+              "r"(p_tile_in + 2 * HMX_FP16_TILE_N_ELMS), "r"(v_tile_in + 2 * HMX_FP16_TILE_N_ELMS),
+              "r"(p_tile_in + 3 * HMX_FP16_TILE_N_ELMS), "r"(v_tile_in + 3 * HMX_FP16_TILE_N_ELMS)
+        );
     } else if (n_col_tiles == 8) {
-        Q6_activation_hf_mxmem_RR((unsigned int) p_tile_in, 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) v_tile_in, 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (p_tile_in + HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (v_tile_in + HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (p_tile_in + 2 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (v_tile_in + 2 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (p_tile_in + 3 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (v_tile_in + 3 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (p_tile_in + 4 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (v_tile_in + 4 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (p_tile_in + 5 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (v_tile_in + 5 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (p_tile_in + 6 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (v_tile_in + 6 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_activation_hf_mxmem_RR((unsigned int) (p_tile_in + 7 * HMX_FP16_TILE_N_ELMS), 2047);
-        Q6_weight_hf_mxmem_RR((unsigned int) (v_tile_in + 7 * HMX_FP16_TILE_N_ELMS), 2047);
+        asm volatile(
+            "{\n"
+            "    activation.hf = mxmem(%0, %2)\n"
+            "    weight.hf = mxmem(%1, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%3, %2)\n"
+            "    weight.hf = mxmem(%4, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%5, %2)\n"
+            "    weight.hf = mxmem(%6, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%7, %2)\n"
+            "    weight.hf = mxmem(%8, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%9, %2)\n"
+            "    weight.hf = mxmem(%10, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%11, %2)\n"
+            "    weight.hf = mxmem(%12, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%13, %2)\n"
+            "    weight.hf = mxmem(%14, %2)\n"
+            "}\n"
+            "{\n"
+            "    activation.hf = mxmem(%15, %2)\n"
+            "    weight.hf = mxmem(%16, %2)\n"
+            "}\n"
+            :
+            : "r"(p_tile_in), "r"(v_tile_in), "r"(2047),
+              "r"(p_tile_in + HMX_FP16_TILE_N_ELMS), "r"(v_tile_in + HMX_FP16_TILE_N_ELMS),
+              "r"(p_tile_in + 2 * HMX_FP16_TILE_N_ELMS), "r"(v_tile_in + 2 * HMX_FP16_TILE_N_ELMS),
+              "r"(p_tile_in + 3 * HMX_FP16_TILE_N_ELMS), "r"(v_tile_in + 3 * HMX_FP16_TILE_N_ELMS),
+              "r"(p_tile_in + 4 * HMX_FP16_TILE_N_ELMS), "r"(v_tile_in + 4 * HMX_FP16_TILE_N_ELMS),
+              "r"(p_tile_in + 5 * HMX_FP16_TILE_N_ELMS), "r"(v_tile_in + 5 * HMX_FP16_TILE_N_ELMS),
+              "r"(p_tile_in + 6 * HMX_FP16_TILE_N_ELMS), "r"(v_tile_in + 6 * HMX_FP16_TILE_N_ELMS),
+              "r"(p_tile_in + 7 * HMX_FP16_TILE_N_ELMS), "r"(v_tile_in + 7 * HMX_FP16_TILE_N_ELMS)
+        );
     } else {
         for (size_t k = 0; k < n_col_tiles; ++k) {
-            Q6_activation_hf_mxmem_RR((unsigned int) p_tile_in, 2047);
-            Q6_weight_hf_mxmem_RR((unsigned int) v_tile_in, 2047);
+            asm volatile(
+                "{\n"
+                "    activation.hf = mxmem(%0, %2)\n"
+                "    weight.hf = mxmem(%1, %2)\n"
+                "}\n"
+                :
+                : "r"(p_tile_in), "r"(v_tile_in), "r"(2047)
+            );
             p_tile_in += HMX_FP16_TILE_N_ELMS;
             v_tile_in += HMX_FP16_TILE_N_ELMS;
         }
     }
-    asm volatile("" ::: "memory");
-    Q6_mxmem_AR_after_hf(o_tile_out, 0);
-    asm volatile("" ::: "memory");
+    asm volatile(
+        "mxmem(%0, %1):after.hf = acc\n"
+        :
+        : "r"(o_tile_out), "r"(0)
+        : "memory"
+    );
 }
 
 static inline void hmx_fa_o_norm_tile(
@@ -158,12 +282,20 @@ static inline void hmx_fa_o_norm_tile(
     const __fp16 * o_rc,
     __fp16 *       o_out
 ) {
-    asm volatile("" ::: "memory");
-    Q6_activation_hf_mxmem_RR((unsigned int) d_diag, 2047);
-    Q6_weight_hf_mxmem_RR((unsigned int) o_rc, 2047);
-    asm volatile("" ::: "memory");
-    Q6_mxmem_AR_after_hf(o_out, 0);
-    asm volatile("" ::: "memory");
+    asm volatile(
+        "{\n"
+        "    activation.hf = mxmem(%0, %2)\n"
+        "    weight.hf = mxmem(%1, %2)\n"
+        "}\n"
+        :
+        : "r"(d_diag), "r"(o_rc), "r"(2047)
+    );
+    asm volatile(
+        "mxmem(%0, %1):after.hf = acc\n"
+        :
+        : "r"(o_out), "r"(0)
+        : "memory"
+    );
 }
 
 #endif /* HMX_FA_KERNELS_H */
