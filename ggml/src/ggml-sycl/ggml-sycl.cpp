e1989e53ce347c3d16d9b5c60ddb556f09a05c9f
hmscider <hmscider@users.noreply.github.com>
2026-07-03 07:30:03 -0400

    PR-25222 revision v2: addressed audits

--- ggml/src/ggml-sycl/ggml-sycl.cpp
+++ ggml/src/ggml-sycl/ggml-sycl.cpp
@@ -355,6 +355,7 @@ static void ggml_check_sycl() try {
         GGML_LOG_INFO("  GGML_SYCL_FA_ONEDNN: %d\n", g_ggml_sycl_fa_onednn);
 #else
         GGML_LOG_INFO("  GGML_SYCL_ENABLE_DNN: DNN disabled by compile flag\n");
+        GGML_LOG_INFO("  GGML_SYCL_FA_ONEDNN: %d\n", g_ggml_sycl_fa_onednn);
 #endif
 
 #ifdef SYCL_FLASH_ATTN
