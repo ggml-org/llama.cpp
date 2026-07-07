e5ff7a8d8fea9da3417b6cf33337e658cc769536
Viacheslav Barinov <vvadbarinov@sberbank.ru>
2026-07-08 02:52:02 +0300

    fix reasoning parser problem

--- common/chat.cpp
+++ common/chat.cpp
@@ -2018,7 +2018,7 @@ static autoparser::generation_params common_chat_gigachat35_render_inputs(
         render_inputs.extra_context = json::object();
     }
     if (!render_inputs.extra_context.contains("reasoning")) {
-        render_inputs.extra_context["reasoning"] = render_inputs.enable_thinking;
+        render_inputs.extra_context["reasoning"] = false;
     }
     return render_inputs;
 }
