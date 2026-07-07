984ac6b8d1f5289cba7f0c30d7dba98788a7c65e
Ruben Ortlam <rortlam@redhat.com>
2026-07-07 14:48:02 +0200

    vulkan: use spec constant for mul mat type_a
    vulkan: use map for mul_mm shapes
    
    cleanup
    
    fix indentation
    
    fix cm2 and shmem init
    
    fix cm2 spec constants
    
    fix cm2 bindings
    
    consolidate shmem tables and reduce size by type spec constant
    
    fix compiler warning
    
    fix missing Q2_0 type
    
    fix unused warning when integer dot glslc support is missing
    
    use minimal shmem size 8 instead of 1 to workaround cm2 compiler bug
    
    fix missing Q2_0 type in cm2 matmul
    
    fix types

--- ggml/src/ggml-vulkan/vulkan-shaders/dequant_funcs_cm2.glsl
+++ ggml/src/ggml-vulkan/vulkan-shaders/dequant_funcs_cm2.glsl
@@ -817,7 +817,7 @@ f16vec4 dequantFuncQ6_K_v(const in decodeBufQ6_K bl, const in uint blockCoords[2
     return f16vec4((vec4(qi) - vec4(32.0f)) * vec4(float(dscale)));
 }
 
-#if defined(DATA_A_IQ1_S)
+#if defined(DATA_A_IQ1_S) || defined(MULMAT_QUANT)
 layout(buffer_reference, std430, buffer_reference_align = 2) buffer decodeBufIQ1_S {
    block_iq1_s block;
 };
@@ -864,7 +864,7 @@ f16vec4 dequantFuncIQ1_S_v(const in decodeBufIQ1_S bl, const in uint blockCoords
 }
 #endif
 
-#if defined(DATA_A_IQ1_M)
+#if defined(DATA_A_IQ1_M) || defined(MULMAT_QUANT)
 layout(buffer_reference, std430, buffer_reference_align = 2) buffer decodeBufIQ1_M {
    block_iq1_m block;
 };
@@ -923,7 +923,7 @@ f16vec4 dequantFuncIQ1_M_v(const in decodeBufIQ1_M bl, const in uint blockCoords
 }
 #endif
 
-#if defined(DATA_A_IQ2_XXS)
+#if defined(DATA_A_IQ2_XXS) || defined(MULMAT_QUANT)
 layout(buffer_reference, std430, buffer_reference_align = 2) buffer decodeBufIQ2_XXS {
    block_iq2_xxs block;
 };
@@ -949,7 +949,7 @@ float16_t dequantFuncIQ2_XXS(const in decodeBufIQ2_XXS bl, const in uint blockCo
     uint sign = bitfieldExtract(signscale, 7 * int(ib8), 7);
     sign |= bitCount(sign) << 7;
 
-    uint g2 = iq2xxs_grid[qs][(idx & 4) >> 2];
+    uint g2 = iq2_grid[qs][(idx & 4) >> 2];
     g2 >>= (idx & 2) * 8;
     const vec2 g = vec2(unpack8(g2));
 
@@ -974,7 +974,7 @@ f16vec4 dequantFuncIQ2_XXS_v(const in decodeBufIQ2_XXS bl, const in uint blockCo
     sign |= bitCount(sign) << 7;
     const uint sb = sign >> (idx & 7u);
 
-    const uint   g2 = iq2xxs_grid[qs][(idx & 4) >> 2];
+    const uint   g2 = iq2_grid[qs][(idx & 4) >> 2];
     const u8vec4 g  = unpack8(g2);
 
     return f16vec4(
@@ -985,7 +985,7 @@ f16vec4 dequantFuncIQ2_XXS_v(const in decodeBufIQ2_XXS bl, const in uint blockCo
 }
 #endif
 
-#if defined(DATA_A_IQ2_XS)
+#if defined(DATA_A_IQ2_XS) || defined(MULMAT_QUANT)
 layout(buffer_reference, std430, buffer_reference_align = 2) buffer decodeBufIQ2_XS {
    block_iq2_xs block;
 };
@@ -1004,7 +1004,7 @@ float16_t dequantFuncIQ2_XS(const in decodeBufIQ2_XS bl, const in uint blockCoor
 
     uint sign = uint(qs >> 9);
     sign |= bitCount(sign) << 7;
-    uint g2 = iq2xs_grid[qs & 0x1FF][(idx & 4) >> 2];
+    uint g2 = iq2_grid[qs & 0x1FF][(idx & 4) >> 2];
     g2 >>= (idx & 2) * 8;
     const vec2 g = vec2(unpack8(g2));
 
@@ -1027,7 +1027,7 @@ f16vec4 dequantFuncIQ2_XS_v(const in decodeBufIQ2_XS bl, const in uint blockCoor
     sign |= bitCount(sign) << 7;
     const uint sb = sign >> (idx & 7u);
 
-    const uint   g2 = iq2xs_grid[qs & 0x1FF][(idx & 4) >> 2];
+    const uint   g2 = iq2_grid[qs & 0x1FF][(idx & 4) >> 2];
     const u8vec4 g  = unpack8(g2);
 
     return f16vec4(
@@ -1038,7 +1038,7 @@ f16vec4 dequantFuncIQ2_XS_v(const in decodeBufIQ2_XS bl, const in uint blockCoor
 }
 #endif
 
-#if defined(DATA_A_IQ2_S)
+#if defined(DATA_A_IQ2_S) || defined(MULMAT_QUANT)
 layout(buffer_reference, std430, buffer_reference_align = 2) buffer decodeBufIQ2_S {
    block_iq2_s block;
 };
@@ -1054,12 +1054,12 @@ float16_t dequantFuncIQ2_S(const in decodeBufIQ2_S bl, const in uint blockCoords
     const uint scale = (bl.block.scales[ib32] >> ((idx & 0x10) >> 2)) & 0xf;
     const uint qs = bl.block.qs[ib8];
     const uint qh = bl.block.qh[ib32];
-    const uint sign = bl.block.qs[QUANT_K / 8 + ib8] >> (idx & 0x6);
+    const uint sign = bl.block.qs[QUANT_K_IQ2_S / 8 + ib8] >> (idx & 0x6);
 
     const float d = float(bl.block.d);
     const float db = d * 0.25 * (0.5 + scale);
     const ivec2 sign01 = 1 - (2 & ivec2(sign << 1, sign));
-    uint g2 = iq2s_grid[qs | ((qh << (8 - qhshift)) & 0x300)][(idx & 4) >> 2];
+    uint g2 = iq2_grid[qs | ((qh << (8 - qhshift)) & 0x300)][(idx & 4) >> 2];
     g2 >>= (idx & 2) * 8;
     const vec2 v = db * vec2(sign01) * vec2(unpack8(g2));
     return float16_t(v[idx & 1]);
@@ -1076,12 +1076,12 @@ f16vec4 dequantFuncIQ2_S_v(const in decodeBufIQ2_S bl, const in uint blockCoords
     const uint scale = (bl.block.scales[ib32] >> ((idx & 0x10) >> 2)) & 0xf;
     const uint qs    = bl.block.qs[ib8];
     const uint qh    = bl.block.qh[ib32];
-    const uint sb    = uint(bl.block.qs[QUANT_K / 8 + ib8]) >> (idx & 0x6u);
+    const uint sb    = uint(bl.block.qs[QUANT_K_IQ2_S / 8 + ib8]) >> (idx & 0x6u);
 
     const float d  = float(bl.block.d);
     const float db = d * 0.25 * (0.5 + scale);
 
-    const uint   g2 = iq2s_grid[qs | ((qh << (8 - qhshift)) & 0x300)][(idx & 4) >> 2];
+    const uint   g2 = iq2_grid[qs | ((qh << (8 - qhshift)) & 0x300)][(idx & 4) >> 2];
     const u8vec4 g  = unpack8(g2);
 
     return f16vec4(
@@ -1092,7 +1092,7 @@ f16vec4 dequantFuncIQ2_S_v(const in decodeBufIQ2_S bl, const in uint blockCoords
 }
 #endif
 
-#if defined(DATA_A_IQ3_XXS)
+#if defined(DATA_A_IQ3_XXS) || defined(MULMAT_QUANT)
 layout(buffer_reference, std430, buffer_reference_align = 2) buffer decodeBufIQ3_XXS {
    block_iq3_xxs block;
 };
@@ -1107,7 +1107,7 @@ float16_t dequantFuncIQ3_XXS(const in decodeBufIQ3_XXS bl, const in uint blockCo
     uint idx = coordInBlock[1];
 
     const uint iqs = (idx & 0xFC) >> 2;             // 0..63
-    const uint is = QUANT_K / 4 + ((idx & 0xE0) >> 3);// 8 values
+    const uint is = QUANT_K_IQ3_XXS / 4 + ((idx & 0xE0) >> 3);// 8 values
 
     const float d = float(bl.block.d);
     const uint qs = bl.block.qs[iqs];
@@ -1119,7 +1119,7 @@ float16_t dequantFuncIQ3_XXS(const in decodeBufIQ3_XXS bl, const in uint blockCo
     const uint32_t sign7 = bitfieldExtract(signs, 7 * (int(iqs / 2) % 4), 7);
     const uint sign = (sign7 | (bitCount(sign7) << 7)) >> (idx & 0x6);
     const ivec2 sign01 = ivec2(1 - (2 & ivec2(sign << 1, sign)));
-    const uint grid = iq3xxs_grid[qs] >> (16 * ((idx & 2) >> 1));
+    const uint grid = iq3_grid[qs] >> (16 * ((idx & 2) >> 1));
     const vec2 v = db * vec2(sign01) * vec2(unpack8(grid).xy);
     return float16_t(v[idx & 1]);
 }
@@ -1130,7 +1130,7 @@ f16vec4 dequantFuncIQ3_XXS_v(const in decodeBufIQ3_XXS bl, const in uint blockCo
     const uint idx = coordInBlock[1];
 
     const uint iqs = idx >> 2;
-    const uint is  = QUANT_K / 4 + ((idx & 0xE0) >> 3);
+    const uint is  = QUANT_K_IQ3_XXS / 4 + ((idx & 0xE0) >> 3);
 
     const float d     = float(bl.block.d);
     const uint  qs    = bl.block.qs[iqs];
@@ -1140,7 +1140,7 @@ f16vec4 dequantFuncIQ3_XXS_v(const in decodeBufIQ3_XXS bl, const in uint blockCo
     const uint sign7 = bitfieldExtract(signs, 7 * (int(iqs / 2) % 4), 7);
     const uint sb    = (sign7 | (bitCount(sign7) << 7)) >> (idx & 0x6u);
 
-    const uint   grid = iq3xxs_grid[qs];
+    const uint   grid = iq3_grid[qs];
     const u8vec4 g    = unpack8(grid);
 
     return f16vec4(
@@ -1151,7 +1151,7 @@ f16vec4 dequantFuncIQ3_XXS_v(const in decodeBufIQ3_XXS bl, const in uint blockCo
 }
 #endif
 
-#if defined(DATA_A_IQ3_S)
+#if defined(DATA_A_IQ3_S) || defined(MULMAT_QUANT)
 layout(buffer_reference, std430, buffer_reference_align = 2) buffer decodeBufIQ3_S {
    block_iq3_s block;
 };
@@ -1170,7 +1170,7 @@ float16_t dequantFuncIQ3_S(const in decodeBufIQ3_S bl, const in uint blockCoords
     const uint scale = bl.block.scales[iqs / 16];
     const ivec2 sign01 = ivec2(1 - (2 & ivec2(sign << 1, sign)));
     const float db = d * (1 + 2 * ((scale >> (4 * (iqh & 1))) & 0xf));
-    const uint32_t grid = iq3s_grid[qs | ((qh << (8 - (iqs % 8))) & 256)] >> ((idx & 2) << 3);
+    const uint32_t grid = iq3_grid[qs | ((qh << (8 - (iqs % 8))) & 256)] >> ((idx & 2) << 3);
     const vec2 v = db * vec2(sign01) * vec2(unpack8(grid).xy);
 
     return float16_t(v[idx & 1]);
@@ -1190,7 +1190,7 @@ f16vec4 dequantFuncIQ3_S_v(const in decodeBufIQ3_S bl, const in uint blockCoords
     const uint  scale = bl.block.scales[iqs / 16];
     const float db    = d * (1 + 2 * ((scale >> (4 * (iqh & 1))) & 0xf));
 
-    const uint   grid = iq3s_grid[qs | ((qh << (8 - (iqs % 8))) & 256)];
+    const uint   grid = iq3_grid[qs | ((qh << (8 - (iqs % 8))) & 256)];
     const u8vec4 g    = unpack8(grid);
 
     return f16vec4(
@@ -1201,7 +1201,7 @@ f16vec4 dequantFuncIQ3_S_v(const in decodeBufIQ3_S bl, const in uint blockCoords
 }
 #endif
 
-#if defined(DATA_A_IQ4_XS)
+#if defined(DATA_A_IQ4_XS) || defined(MULMAT_QUANT)
 layout(buffer_reference, std430, buffer_reference_align = 2) buffer decodeBufIQ4_XS {
    block_iq4_xs block;
 };
@@ -1251,7 +1251,7 @@ f16vec4 dequantFuncIQ4_XS_v(const in decodeBufIQ4_XS bl, const in uint blockCoor
 }
 #endif
 
-#if defined(DATA_A_IQ4_NL)
+#if defined(DATA_A_IQ4_NL) || defined(MULMAT_QUANT)
 layout(buffer_reference, std430, buffer_reference_align = 2) buffer decodeBufIQ4_NL {
    block_iq4_nl block;
 };
@@ -1292,7 +1292,7 @@ f16vec4 dequantFuncIQ4_NL_v(const in decodeBufIQ4_NL bl, const in uint blockCoor
 }
 #endif
 
-#if defined(DATA_A_MXFP4)
+#if defined(DATA_A_MXFP4) || defined(MULMAT_QUANT)
 layout(buffer_reference, std430, buffer_reference_align = 2) buffer decodeBufMXFP4 {
    block_mxfp4 block;
 };
@@ -1346,7 +1346,7 @@ f16vec4 dequantFuncMXFP4_v(const in decodeBufMXFP4 bl, const in uint blockCoords
 }
 #endif
 
-#if defined(DATA_A_NVFP4)
+#if defined(DATA_A_NVFP4) || defined(MULMAT_QUANT)
 layout(buffer_reference, std430, buffer_reference_align = 4) buffer decodeBufNVFP4 {
    block_nvfp4 block;
 };
