5d5e7d3ec919f5ecbd2c640f832970ad06bba47d
Matti4 <ristorim013@gmail.com>
2026-07-07 17:11:04 +0200

    Refactor htp_opnode structure and methods

--- ggml/src/ggml-hexagon/htp-opnode.h
+++ ggml/src/ggml-hexagon/htp-opnode.h
@@ -5,13 +5,10 @@
 #include "ggml-backend-impl.h"
 #include "ggml-common.h"
 
-#include <algorithm>
 #include <string>
 #include <vector>
 #include <stdio.h>
 #include "htp-ops.h"
-#include "htp/matmul-ops.h"
-#include "htp/flash-attn-ops.h"
 
 struct htp_opnode {
     ggml_tensor * node = nullptr;
@@ -20,39 +17,12 @@ struct htp_opnode {
 
     htp_op_code opcode = HTP_OP_INVALID;
 
-    std::vector<ggml_tensor *> extra_dsts;
-
-    int32_t kernel_params[HTP_OP_MAX_KERN_PARAMS] = {0};
-
-    htp_opnode(ggml_tensor * node = nullptr, std::vector<ggml_tensor *> fused = {}, htp_op_code opcode = HTP_OP_INVALID, std::vector<ggml_tensor *> extra_dsts = {})
-        : node(node), fused(std::move(fused)), opcode(opcode), extra_dsts(std::move(extra_dsts)) {}
-
     ggml_op op() const {
         return node->op;
     }
 
     const ggml_tensor * dst() const {
         return fused.empty() ? node : fused.back();
-    }
-
-    void add_fused(ggml_tensor * t, bool extra_dst = false) {
-        fused.push_back(t);
-        if (extra_dst) {
-            extra_dsts.push_back(t);
-        }
-    }
-
-    std::vector<const ggml_tensor *> get_outputs() const {
-        std::vector<const ggml_tensor *> res;
-        if (extra_dsts.empty()) {
-            res.push_back(dst());
-        } else {
-            res.push_back(node);
-            for (const auto * x : extra_dsts) {
-                res.push_back(x);
-            }
-        }
-        return res;
     }
 
     const ggml_tensor * src0() const {
@@ -65,6 +35,10 @@ struct htp_opnode {
 
     bool is_empty() const {
         return ggml_op_is_empty(node->op);
+    }
+
+    void add_fused(ggml_tensor * t) {
+        fused.push_back(t);
     }
 
     bool stackable() const {
@@ -157,213 +131,234 @@ struct htp_opformat {
     char types[16 * GGML_MAX_SRC];
     char buffs[64 * GGML_MAX_SRC];
     char names[64 * GGML_MAX_SRC];
-    char kparams[128];
 
-    int format_tensor_dims(char * str, size_t max_size, const struct ggml_tensor * t) {
+    int format_tensor_dims(char * str, size_t max_len, const struct ggml_tensor * t) {
         if (!t) {
-            return snprintf(str, max_size, "NONE");
+            return snprintf(str, max_len, "NONE");
         }
         if (t->ne[2] == 1 && t->ne[3] == 1) {
-            return snprintf(str, max_size, "%d:%d", (int) t->ne[0], (int) t->ne[1]);
+            return snprintf(str, max_len, "%d:%d", (int) t->ne[0], (int) t->ne[1]);
         } else {
-            return snprintf(str, max_size, "%d:%d:%d:%d", (int) t->ne[0], (int) t->ne[1], (int) t->ne[2], (int) t->ne[3]);
+            return snprintf(str, max_len, "%d:%d:%d:%d", (int) t->ne[0], (int) t->ne[1], (int) t->ne[2], (int) t->ne[3]);
         }
     }
 
-    void format_op_dims(char * str, size_t max_size, const htp_opnode & node) {
+    void format_op_dims(char * str, size_t max_len, const htp_opnode & node) {
         char * p = str;
-        char * p_end = str + max_size;
+        size_t remaining = max_len;
         auto inputs = node.get_inputs();
 
         if (!inputs.empty()) {
-            p += std::min((size_t)format_tensor_dims(p, p_end - p, inputs[0]), (size_t)(p_end - p));
-
-            for (size_t i = 1; i < inputs.size(); i++) {
-                if (p < p_end) {
-                    p += std::min((size_t)snprintf(p, p_end - p, " x "), (size_t)(p_end - p));
-                }
-                if (p < p_end) {
-                    p += std::min((size_t)format_tensor_dims(p, p_end - p, inputs[i]), (size_t)(p_end - p));
-                }
+            int written = format_tensor_dims(p, remaining, inputs[0]);
+            if (written > 0) {
+                p += written;
+                remaining -= written;
             }
 
-            if (p < p_end) {
-                p += std::min((size_t)snprintf(p, p_end - p, " -> "), (size_t)(p_end - p));
+            for (size_t i = 1; i < inputs.size() && remaining > 0; i++) {
+                int w_sep = snprintf(p, remaining, " x ");
+                if (w_sep > 0) {
+                    p += w_sep;
+                    remaining -= w_sep;
+                }
+                int w_val = format_tensor_dims(p, remaining, inputs[i]);
+                if (w_val > 0) {
+                    p += w_val;
+                    remaining -= w_val;
+                }
+            }
+            int w_arrow = snprintf(p, remaining, " -> ");
+            if (w_arrow > 0) {
+                p += w_arrow;
+                remaining -= w_arrow;
             }
         }
 
         char self[64];
         format_tensor_dims(self, sizeof(self), node.dst());
-        if (p < p_end) {
-            p += std::min((size_t)snprintf(p, p_end - p, "%s", self), (size_t)(p_end - p));
-        }
+        snprintf(p, remaining, "%s", self);
     }
 
-    int format_tensor_strides(char * str, size_t max_size, const struct ggml_tensor * t) {
+    int format_tensor_strides(char * str, size_t max_len, const struct ggml_tensor * t) {
         if (!t) {
-            return snprintf(str, max_size, "NONE");
+            return snprintf(str, max_len, "NONE");
         }
         const char * c = ggml_is_contiguous(t) ? "" : "!";
-
         if (t->ne[2] == 1 && t->ne[3] == 1) {
-            return snprintf(str, max_size, "%zu:%zu%s", (size_t) t->nb[0], (size_t) t->nb[1], c);
+            return snprintf(str, max_len, "%zu:%zu%s", (size_t) t->nb[0], (size_t) t->nb[1], c);
         } else {
-            return snprintf(str, max_size, "%zu:%zu:%zu:%zu%s", (size_t) t->nb[0], (size_t) t->nb[1], (size_t) t->nb[2], (size_t) t->nb[3], c);
+            return snprintf(str, max_len, "%zu:%zu:%zu:%zu%s", (size_t) t->nb[0], (size_t) t->nb[1], (size_t) t->nb[2], (size_t) t->nb[3], c);
         }
     }
 
-    void format_op_strides(char * str, size_t max_size, const htp_opnode & node) {
+    void format_op_strides(char * str, size_t max_len, const htp_opnode & node) {
         char * p = str;
-        char * p_end = str + max_size;
+        size_t remaining = max_len;
         auto inputs = node.get_inputs();
 
         if (!inputs.empty()) {
-            p += std::min((size_t)format_tensor_strides(p, p_end - p, inputs[0]), (size_t)(p_end - p));
-
-            for (size_t i = 1; i < inputs.size(); i++) {
-                if (p < p_end) {
-                    p += std::min((size_t)snprintf(p, p_end - p, " x "), (size_t)(p_end - p));
-                }
-                if (p < p_end) {
-                    p += std::min((size_t)format_tensor_strides(p, p_end - p, inputs[i]), (size_t)(p_end - p));
-                }
+            int written = format_tensor_strides(p, remaining, inputs[0]);
+            if (written > 0) {
+                p += written;
+                remaining -= written;
             }
 
-            if (p < p_end) {
-                p += std::min((size_t)snprintf(p, p_end - p, " -> "), (size_t)(p_end - p));
+            for (size_t i = 1; i < inputs.size() && remaining > 0; i++) {
+                int w_sep = snprintf(p, remaining, " x ");
+                if (w_sep > 0) {
+                    p += w_sep;
+                    remaining -= w_sep;
+                }
+                int w_val = format_tensor_strides(p, remaining, inputs[i]);
+                if (w_val > 0) {
+                    p += w_val;
+                    remaining -= w_val;
+                }
+            }
+            int w_arrow = snprintf(p, remaining, " -> ");
+            if (w_arrow > 0) {
+                p += w_arrow;
+                remaining -= w_arrow;
             }
         }
 
         char self[64];
         format_tensor_strides(self, sizeof(self), node.dst());
-        if (p < p_end) {
-            p += std::min((size_t)snprintf(p, p_end - p, "%s", self), (size_t)(p_end - p));
-        }
+        snprintf(p, remaining, "%s", self);
     }
 
-    void format_op_types(char * str, size_t max_size, const htp_opnode & node) {
+    int format_tensor_types(char * str, size_t max_len, const struct ggml_tensor * t) {
+        if (!t) {
+            return snprintf(str, max_len, "NONE");
+        }
+        return snprintf(str, max_len, "%s", ggml_type_name(t->type));
+    }
+
+    void format_op_types(char * str, size_t max_len, const htp_opnode & node) {
         char * p = str;
-        char * p_end = str + max_size;
+        size_t remaining = max_len;
         auto inputs = node.get_inputs();
 
         if (!inputs.empty()) {
-            if (p < p_end) {
-                p += std::min((size_t)snprintf(p, p_end - p, "%s", inputs[0] ? ggml_type_name(inputs[0]->type) : "NONE"), (size_t)(p_end - p));
+            int written = format_tensor_types(p, remaining, inputs[0]);
+            if (written > 0) {
+                p += written;
+                remaining -= written;
             }
 
-            for (size_t i = 1; i < inputs.size(); i++) {
-                if (p < p_end) {
-                    p += std::min((size_t)snprintf(p, p_end - p, " x "), (size_t)(p_end - p));
+            for (size_t i = 1; i < inputs.size() && remaining > 0; i++) {
+                int w_sep = snprintf(p, remaining, " x ");
+                if (w_sep > 0) {
+                    p += w_sep;
+                    remaining -= w_sep;
                 }
-                if (p < p_end) {
-                    p += std::min((size_t)snprintf(p, p_end - p, "%s", inputs[i] ? ggml_type_name(inputs[i]->type) : "NONE"), (size_t)(p_end - p));
+                int w_val = format_tensor_types(p, remaining, inputs[i]);
+                if (w_val > 0) {
+                    p += w_val;
+                    remaining -= w_val;
                 }
             }
-
-            if (p < p_end) {
-                p += std::min((size_t)snprintf(p, p_end - p, " -> "), (size_t)(p_end - p));
+            int w_arrow = snprintf(p, remaining, " -> ");
+            if (w_arrow > 0) {
+                p += w_arrow;
+                remaining -= w_arrow;
             }
         }
 
-        if (p < p_end) {
-            p += std::min((size_t)snprintf(p, p_end - p, "%s", ggml_type_name(node.dst()->type)), (size_t)(p_end - p));
+        int written_dst = format_tensor_types(p, remaining, node.dst());
+        if (written_dst > 0) {
+            p += written_dst;
+            remaining -= written_dst;
         }
     }
 
-    const char * tensor_buff_name(const struct ggml_tensor * t) {
+    int format_tensor_buff_name(char * str, size_t max_len, const struct ggml_tensor * t) {
         if (t && t->buffer) {
-            return ggml_backend_buffer_name(t->buffer);
+            return snprintf(str, max_len, "%s", ggml_backend_buffer_name(t->buffer));
         }
-        return "NONE";
+        return snprintf(str, max_len, "NONE");
     }
 
-    void format_op_buffs(char * str, size_t max_size, const htp_opnode & node) {
+    void format_op_buffs(char * str, size_t max_len, const htp_opnode & node) {
         char * p = str;
-        char * p_end = str + max_size;
+        size_t remaining = max_len;
         auto inputs = node.get_inputs();
 
         if (!inputs.empty()) {
-            if (p < p_end) {
-                p += std::min((size_t)snprintf(p, p_end - p, "%s", tensor_buff_name(inputs[0])), (size_t)(p_end - p));
+            int written = format_tensor_buff_name(p, remaining, inputs[0]);
+            if (written > 0) {
+                p += written;
+                remaining -= written;
             }
 
-            for (size_t i = 1; i < inputs.size(); i++) {
-                if (p < p_end) {
-                    p += std::min((size_t)snprintf(p, p_end - p, " x "), (size_t)(p_end - p));
+            for (size_t i = 1; i < inputs.size() && remaining > 0; i++) {
+                int w_sep = snprintf(p, remaining, " x ");
+                if (w_sep > 0) {
+                    p += w_sep;
+                    remaining -= w_sep;
                 }
-                if (p < p_end) {
-                    p += std::min((size_t)snprintf(p, p_end - p, "%s", tensor_buff_name(inputs[i])), (size_t)(p_end - p));
+                int w_val = format_tensor_buff_name(p, remaining, inputs[i]);
+                if (w_val > 0) {
+                    p += w_val;
+                    remaining -= w_val;
                 }
             }
-
-            if (p < p_end) {
-                p += std::min((size_t)snprintf(p, p_end - p, " -> "), (size_t)(p_end - p));
+            int w_arrow = snprintf(p, remaining, " -> ");
+            if (w_arrow > 0) {
+                p += w_arrow;
+                remaining -= w_arrow;
             }
         }
 
-        if (p < p_end) {
-            p += std::min((size_t)snprintf(p, p_end - p, "%s", tensor_buff_name(node.dst())), (size_t)(p_end - p));
+        int written_dst = format_tensor_buff_name(p, remaining, node.dst());
+        if (written_dst > 0) {
+            p += written_dst;
+            remaining -= written_dst;
         }
     }
 
-    void format_op_names(char * str, size_t max_size, const htp_opnode & node) {
+    int format_tensor_name(char * str, size_t max_len, const struct ggml_tensor * t) {
+        if (t) {
+            return snprintf(str, max_len, "%s", t->name);
+        }
+        return snprintf(str, max_len, "NONE");
+    }
+
+    void format_op_names(char * str, size_t max_len, const htp_opnode & node) {
         char * p = str;
-        char * p_end = str + max_size;
+        size_t remaining = max_len;
         auto inputs = node.get_inputs();
 
         if (!inputs.empty()) {
-            if (p < p_end) {
-                p += std::min((size_t)snprintf(p, p_end - p, "%s", inputs[0] ? inputs[0]->name : "NONE"), (size_t)(p_end - p));
+            int written = format_tensor_name(p, remaining, inputs[0]);
+            if (written > 0) {
+                p += written;
+                remaining -= written;
             }
 
-            for (size_t i = 1; i < inputs.size(); i++) {
-                if (p < p_end) {
-                    p += std::min((size_t)snprintf(p, p_end - p, " x "), (size_t)(p_end - p));
+            for (size_t i = 1; i < inputs.size() && remaining > 0; i++) {
+                int w_sep = snprintf(p, remaining, " x ");
+                if (w_sep > 0) {
+                    p += w_sep;
+                    remaining -= w_sep;
                 }
-                if (p < p_end) {
-                    p += std::min((size_t)snprintf(p, p_end - p, "%s", inputs[i] ? inputs[i]->name : "NONE"), (size_t)(p_end - p));
+                int w_val = format_tensor_name(p, remaining, inputs[i]);
+                if (w_val > 0) {
+                    p += w_val;
+                    remaining -= w_val;
                 }
             }
-
-            if (p < p_end) {
-                p += std::min((size_t)snprintf(p, p_end - p, " -> "), (size_t)(p_end - p));
+            int w_arrow = snprintf(p, remaining, " -> ");
+            if (w_arrow > 0) {
+                p += w_arrow;
+                remaining -= w_arrow;
             }
         }
 
-        if (p < p_end) {
-            p += std::min((size_t)snprintf(p, p_end - p, "%s", node.dst()->name), (size_t)(p_end - p));
-        }
-    }
-    void format_kernel_params(char * str, size_t max_size, const htp_opnode & node) {
-        if (node.opcode == HTP_OP_MUL_MAT || node.opcode == HTP_OP_MUL_MAT_ID ||
-            node.opcode == HTP_OP_MUL_MAT_QKV || node.opcode == HTP_OP_MUL_MAT_FFN ||
-            node.opcode == HTP_OP_MUL_MAT_ADD) {
-            const auto * kparams = (const struct htp_mm_kernel_params *) node.kernel_params;
-            const char * path = "unknown";
-            int32_t type = kparams->kernel_type;
-            if (type == HTP_MM_KERNEL_HMX_2D || type == HTP_MM_KERNEL_HMX_F16_BATCHED) {
-                path = "hmx-tiled";
-            } else if (type == HTP_MM_KERNEL_HVX_F16_F16_VTCM || type == HTP_MM_KERNEL_HVX_F32_F32_VTCM ||
-                       type == HTP_MM_KERNEL_HVX_QUANT_ROW    || type == HTP_MM_KERNEL_HVX_QUANT_BLOCK) {
-                path = "hvx-tiled";
-            } else if (type == HTP_MM_KERNEL_HVX_F16_F16_DDR  || type == HTP_MM_KERNEL_HVX_F16_F32_DDR ||
-                       type == HTP_MM_KERNEL_HVX_F32_F32_DDR  || type == HTP_MM_KERNEL_HVX_F32_F16_DDR ||
-                       type == HTP_MM_KERNEL_HVX_QUANT_ROW_FLAT) {
-                path = "hvx-flat";
-            }
-            snprintf(str, max_size, "%s vtcm %d", path, (int) kparams->vtcm_size);
-        } else if (node.opcode == HTP_OP_FLASH_ATTN_EXT) {
-            const auto * kparams = (const struct htp_fa_kernel_params *) node.kernel_params;
-            const char * path = "unknown";
-            int32_t type = kparams->kernel_type;
-            if (type == HTP_FA_KERNEL_HMX) {
-                path = kparams->u.hmx.pipeline ? "hmx-pipe" : "hmx-seq";
-            } else if (type == HTP_FA_KERNEL_HVX) {
-                path = "hvx";
-            }
-            snprintf(str, max_size, "%s vtcm %d", path, (int) kparams->vtcm_size);
-        } else {
-            snprintf(str, max_size, "----");
+        int written_dst = format_tensor_name(p, remaining, node.dst());
+        if (written_dst > 0) {
+            p += written_dst;
+            remaining -= written_dst;
         }
     }
 
@@ -373,17 +368,9 @@ struct htp_opformat {
         format_op_types(types, sizeof(types), node);
         format_op_buffs(buffs, sizeof(buffs), node);
         format_op_names(names, sizeof(names), node);
-        format_kernel_params(kparams, sizeof(kparams), node);
     }
 
-    htp_opformat() {
-        strides[0] = '\0';
-        dims[0]    = '\0';
-        types[0]   = '\0';
-        buffs[0]   = '\0';
-        names[0]   = '\0';
-        kparams[0] = '\0';
-    }
+    htp_opformat() {}
     htp_opformat(const htp_opnode & node) { format(node); }
 };
 
