dcb682f8d0a15f636ce5a5a81b59b0d6629c3062
Max Krasnyansky <maxk@qti.qualcomm.com>
2026-07-10 14:42:36 -0700

    hex-work: minor cleanup for work-queue apis

--- ggml/src/ggml-hexagon/htp/work-queue.h
+++ ggml/src/ggml-hexagon/htp/work-queue.h
@@ -9,25 +9,18 @@ typedef void (*work_queue_callback_t)(unsigned int n, unsigned int i, void *);
 struct work_queue_s;
 typedef struct work_queue_s * work_queue_t;
 
-#define WORK_QUEUE_THREAD_STACK_SIZE  (2 * 16384)
 #define WORK_QUEUE_MAX_N_THREADS      10
+#define WORK_QUEUE_THREAD_STACK_SIZE  (2 * 16384)
 
 #define WORK_QUEUE_SIZE               16
 #define WORK_QUEUE_MASK               (WORK_QUEUE_SIZE - 1)
 
-#if __HVX_ARCH__ > 69
-#define WORK_QUEUE_POLL_COUNT       2000
-#else
-#define WORK_QUEUE_POLL_COUNT       1
-#endif
+#define WORK_QUEUE_POLL_COUNT         2000
 
 bool work_queue_init(work_queue_t * q, uint32_t n_threads);
 void work_queue_release(work_queue_t * q);
-
-// Async run helper (internal API called by work_queue_run)
 bool work_queue_run_async(work_queue_t q, work_queue_callback_t func, void * data, unsigned int n);
 
-// Main work queue run API
 static inline bool work_queue_run(work_queue_t q, work_queue_callback_t func, void * data, unsigned int n) {
     if (n <= 1) {
         func(n, 0, data);
