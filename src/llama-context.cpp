92160c8fbb22b99859a01204f4840a660c7064e6
ynankani <ynankani@nvidia.com>
2026-07-14 12:02:08 +0000

    Rebase and address review comments
    Signed-off-by: ynankani <ynankani@nvidia.com>

--- src/llama-context.cpp
+++ src/llama-context.cpp
@@ -2401,6 +2401,7 @@ llm_graph_params llama_context::graph_params(
         /*.n_outputs   =*/ n_outputs,
         /*.cb          =*/ graph_get_cb(),
         /*.res         =*/ res,
+        /*.act_policy  =*/ &model.act_policy,
     };
 }
 
