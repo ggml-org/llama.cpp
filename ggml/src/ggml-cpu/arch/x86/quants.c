4cc28ba1ea6c61495608d3ddaae708804ff385b6
Saejin Kim-Heinert <saejinmh@va.wagner.com>
2026-07-30 21:17:19 -0400

    ggml-cpu: add vector-lane accumulation for x86 VNNI q2_0 x q8_0

--- ggml/src/ggml-cpu/arch/x86/quants.c
+++ ggml/src/ggml-cpu/arch/x86/quants.c
@@ -578,11 +578,11 @@ void ggml_vec_dot_q2_0_q8_0(int n, float * GGML_RESTRICT s, size_t bs, const voi
     const __m128i idxhi = _mm_setr_epi8(4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7);
     const __m256i mul   = _mm256_setr_epi16(64,16,4,1, 64,16,4,1, 64,16,4,1, 64,16,4,1);
     const __m256i three = _mm256_set1_epi16(3);
-    float sumf = 0.0f;
+    __m256 acc = _mm256_setzero_ps();
 
     for (int i = 0; i < nb; i++) {
         const float d0 = GGML_CPU_FP16_TO_FP32(x[i].d);
-        float sumi = 0.0f;
+        __m256 acc_block = _mm256_setzero_ps();
         for (int k = 0; k < 2; k++) {
             const block_q8_0 * GGML_RESTRICT yb = &y[i * 2 + k];
             const float d1 = GGML_CPU_FP16_TO_FP32(yb->d);
@@ -594,14 +594,15 @@ void ggml_vec_dot_q2_0_q8_0(int n, float * GGML_RESTRICT s, size_t bs, const voi
             r0 = _mm256_and_si256(_mm256_srli_epi16(_mm256_mullo_epi16(r0, mul), 6), three);
             r1 = _mm256_and_si256(_mm256_srli_epi16(_mm256_mullo_epi16(r1, mul), 6), three);
             const __m256i codes = _mm256_permute4x64_epi64(_mm256_packus_epi16(r0, r1), 0xD8);
-            const int dp = hsum_i32_8(GGML_DPBUSD_256(_mm256_setzero_si256(), codes, qy));
-            const int sy = hsum_i32_8(GGML_DPBUSD_256(_mm256_setzero_si256(), ones, qy));
-            sumi += d1 * (float) (dp - sy);
+            const __m256i dp = GGML_DPBUSD_256(_mm256_setzero_si256(), codes, qy);
+            const __m256i sy = GGML_DPBUSD_256(_mm256_setzero_si256(), ones, qy);
+            const __m256 dot = _mm256_cvtepi32_ps(_mm256_sub_epi32(dp, sy));
+            acc_block = _mm256_add_ps(acc_block, _mm256_mul_ps(_mm256_set1_ps(d1), dot));
         }
-        sumf += d0 * sumi;
+        acc = _mm256_add_ps(acc, _mm256_mul_ps(_mm256_set1_ps(d0), acc_block));
     }
 
-    *s = sumf;
+    *s = hsum_float_8(acc);
 #else
     ggml_vec_dot_q2_0_q8_0_generic(n, s, bs, vx, bx, vy, by, nrc);
 #endif
