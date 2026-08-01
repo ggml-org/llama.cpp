7fb21ab924c3f1c9ddd79370440b98ca25e37b1c
edwinbrowwn <edwin.dev@hotmail.com>
2026-08-01 01:09:27 +0000

    test: use public flash attention parameter

--- tests/test-llama-archs.cpp
+++ tests/test-llama-archs.cpp
@@ -304,7 +304,7 @@ static std::pair<llama_model_ptr, llama_context_ptr> get_model_and_ctx(
         ctx_params.n_ubatch = 64;
     }
     if (getenv("LLAMA_TEST_FLASH_ATTN") != nullptr) {
-        ctx_params.flash_attn = true;
+        ctx_params.flash_attn_type = LLAMA_FLASH_ATTN_TYPE_ENABLED;
     }
 
     size_t tmp = seed;
