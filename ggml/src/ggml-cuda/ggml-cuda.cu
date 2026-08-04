c502c7d6fc3604d5649b02bd4d5b7b54adc14eb2
ZHANG XIANBANG <3157601088@qq.com>
2026-08-04 12:00:45 +0800

    increase indent

--- ggml/src/ggml-cuda/ggml-cuda.cu
+++ ggml/src/ggml-cuda/ggml-cuda.cu
@@ -4767,7 +4767,7 @@ static bool ggml_backend_cuda_device_supports_op(ggml_backend_dev_t dev, const g
                 case GGML_UNARY_OP_FLOOR:
                 case GGML_UNARY_OP_ROUND:
                 case GGML_UNARY_OP_TRUNC:
-                // TODO: should become:
+                    // TODO: should become:
                     //return ggml_is_contiguous_rows(op->src[0]);
                     return ggml_is_contiguous(op->src[0]);
                 case GGML_UNARY_OP_CEIL:
