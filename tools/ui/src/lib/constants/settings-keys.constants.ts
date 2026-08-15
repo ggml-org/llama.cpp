3f7e6974d7bf9b78b2df167050cc283519407496
Aleksander Grygier <aleksander.grygier@gmail.com>
2026-08-15 22:53:48 +0200

    ui : add show-org-name-in-trigger display setting

--- tools/ui/src/lib/constants/settings-keys.constants.ts
+++ tools/ui/src/lib/constants/settings-keys.constants.ts
@@ -57,6 +57,7 @@ export const SETTINGS_KEYS = {
 	SHOW_MODEL_ORG_NAME_IN_TRIGGER: 'showModelOrgNameInTrigger',
 	SHOW_MODEL_QUANTIZATION: 'showModelQuantization',
 	SHOW_MODEL_TAGS: 'showModelTags',
+	SHOW_MODEL_ORG_NAME_IN_TRIGGER: 'showModelOrgNameInTrigger',
 	SHOW_RAW_MODEL_NAMES: 'showRawModelNames',
 	SHOW_RAW_OUTPUT_SWITCH: 'showRawOutputSwitch',
 	SHOW_SYSTEM_MESSAGE: 'showSystemMessage',
