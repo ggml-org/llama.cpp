24added9369cc6530e63203effb3c18890b5faee
007jbks <kartiksirohi383@gmail.com>
2026-08-21 14:19:16 +0530

    ggml: remove redundant concat asserts

--- ggml/src/ggml-cpu/ops.cpp
+++ ggml/src/ggml-cpu/ops.cpp
@@ -2084,13 +2084,6 @@ void ggml_compute_forward_concat(
     const ggml_tensor * src0 = dst->src[0];
     const ggml_tensor * src1 = dst->src[1];
 
-    if (ggml_is_quantized(src0->type)) {
-        GGML_ASSERT(ggml_is_contiguous_rows(src0));
-        GGML_ASSERT(ggml_is_contiguous_rows(src1));
-        GGML_ASSERT(src0->ne[0] % ggml_blck_size(src0->type) == 0);
-        GGML_ASSERT(src1->ne[0] % ggml_blck_size(src1->type) == 0);
-    }
-
     switch (src0->type) {
         case GGML_TYPE_F16:
         case GGML_TYPE_BF16:
