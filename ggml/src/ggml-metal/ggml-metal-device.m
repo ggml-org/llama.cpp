3797e8c4d942e0834706f00b13a88245dec23609
Mads Marquart <mads@marquart.dk>
2026-09-02 11:49:29 +0200

    Write it even shorter
    Co-authored-by: Niklas Wenzel <dev@nikwen.de>

--- ggml/src/ggml-metal/ggml-metal-device.m
+++ ggml/src/ggml-metal/ggml-metal-device.m
@@ -1471,15 +1471,10 @@ void ggml_metal_device_event_synchronize(ggml_metal_device_t dev, ggml_metal_eve
 
 void ggml_metal_device_get_memory(ggml_metal_device_t dev, size_t * free, size_t * total) {
     if (@available(macOS 10.12, iOS 16.0, *)) {
-        *total = dev->mtl_device.recommendedMaxWorkingSetSize;
+        *total     = dev->mtl_device.recommendedMaxWorkingSetSize;
         size_t cur = dev->mtl_device.currentAllocatedSize;
-        if (*total >= cur) {
-            *free  = *total - cur;
-        } else {
-            // We could have allocated above `recommendedMaxWorkingSetSize`,
-            // if that's the case report no free memory.
-            *free  = 0;
-        }
+        // it's possible to allocate more than `recommendedMaxWorkingSetSize`
+        *free      = *total > cur ? *total - cur : 0;
     } else {
         *free = 0;
         *total = 0;
