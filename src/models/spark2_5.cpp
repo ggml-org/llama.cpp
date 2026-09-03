e79b7730bd4b6fb32f28023692bce38df6a4e2a9
KnightYao <KnightYao@users.noreply.github.com>
2026-09-03 19:24:25 +0800

    Update src/models/spark2_5.cpp
    Co-authored-by: Sigbjørn Skjæret <sigbjorn.skjaeret@huggingface.co>

--- src/models/spark2_5.cpp
+++ src/models/spark2_5.cpp
@@ -5,7 +5,7 @@ void llama_model_spark2_5::load_arch_hparams(llama_model_loader & ml) {
     ml.get_key(LLM_KV_ATTENTION_SLIDING_WINDOW, hparams.n_swa);
 
     hparams.swa_type = LLAMA_SWA_TYPE_STANDARD;
-    ml.get_key_or_arr(LLM_KV_ATTENTION_SLIDING_WINDOW_PATTERN, hparams.is_swa_impl, hparams.n_layer());
+    ml.get_arr(LLM_KV_ATTENTION_SLIDING_WINDOW_PATTERN, hparams.is_swa_impl);
 
     hparams.rope_freq_base_train_swa = hparams.rope_freq_base_train;
     hparams.rope_freq_scale_train_swa = hparams.rope_freq_scale_train;
