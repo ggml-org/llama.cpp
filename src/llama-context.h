8114497971ad41798568f257d77dbf330d657c50
Gaurav Garg <gaugarg@nvidia.com>
2026-09-07 19:28:49 +0530

    Rename field

--- src/llama-context.h
+++ src/llama-context.h
@@ -366,7 +366,7 @@ private:
 
     llm_graph_result_ptr gf_res_prev;
     // separate metadata arenas give alternating MTP shapes stable CUDA graph cache keys
-    llm_graph_result_ptr gf_res_prev_mtp_catchup;
+    llm_graph_result_ptr gf_res_prev_mtp_prefill;
     llm_graph_result_ptr gf_res_reserve;
 
     llm_graph_result * gf_res_prev_active = nullptr;
