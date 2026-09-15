18ce15853c135ae5a728cc77a6ff3698c01708f0
Johannes Gäßler <johannesg@5d6.de>
2026-09-15 22:31:06 +0200

    Apply suggestion from @JohannesGaessler

--- ggml/src/ggml-cuda/mmvq.cu
+++ ggml/src/ggml-cuda/mmvq.cu
@@ -345,10 +345,11 @@ bool ggml_cuda_should_use_mmvq(enum ggml_type type, int cc, int64_t ne11) {
             case GGML_TYPE_Q3_K:
                 return ne11 <= 6;
             case GGML_TYPE_Q4_K:
-                return ne11 <= 4;
-            case GGML_TYPE_Q5_K:
-            case GGML_TYPE_Q6_K:
                 return ne11 <= 5;
+            case GGML_TYPE_Q5_K:
+                return ne11 <= 6;
+            case GGML_TYPE_Q6_K:
+                return ne11 <= 7;
             default:
                 return ne11 <= MMVQ_MAX_BATCH_SIZE;
         }
