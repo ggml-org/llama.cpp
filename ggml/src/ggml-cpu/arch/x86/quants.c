305a1da71698a62e80d1a3f6b42863879e948648
AnkitNakhawa <ankitnakhawa@gmail.com>
2026-09-24 07:40:50 +0000

    changes to reduce cpu latency on x86

--- ggml/src/ggml-cpu/arch/x86/quants.c
+++ ggml/src/ggml-cpu/arch/x86/quants.c
@@ -1322,28 +1322,6 @@ void ggml_vec_dot_q8_0_q8_0(int n, float * GGML_RESTRICT s, size_t bs, const voi
     int ib = 0;
     float sumf = 0;
 
-#if defined(__AVX2__) && !__AVXVNNIINT8__
-    for (int i = 0; i < nb; ++i) {
-        const __m256i qy = _mm256_loadu_si256((const __m256i *) y[i].qs);
-        const __m256i y_min = _mm256_cmpeq_epi8(qy, _mm256_set1_epi8(INT8_MIN));
-        if (!_mm256_testz_si256(y_min, y_min)) {
-            ggml_vec_dot_q8_0_q8_0_generic(n, s, bs, vx, bx, vy, by, nrc);
-            return;
-        }
-    }
-#elif defined(__AVX__)
-    const __m128i min = _mm_set1_epi8(INT8_MIN);
-    for (int i = 0; i < nb; ++i) {
-        const __m128i qyl = _mm_loadu_si128((const __m128i *) y[i].qs);
-        const __m128i qyh = _mm_loadu_si128((const __m128i *) y[i].qs + 1);
-        const __m128i y_min = _mm_or_si128(_mm_cmpeq_epi8(qyl, min), _mm_cmpeq_epi8(qyh, min));
-        if (_mm_movemask_epi8(y_min)) {
-            ggml_vec_dot_q8_0_q8_0_generic(n, s, bs, vx, bx, vy, by, nrc);
-            return;
-        }
-    }
-#endif
-
 #if defined(__AVX2__)
     // Initialize accumulator with zeros
     __m256 acc = _mm256_setzero_ps();
@@ -1354,6 +1332,14 @@ void ggml_vec_dot_q8_0_q8_0(int n, float * GGML_RESTRICT s, size_t bs, const voi
         const __m256 d = _mm256_set1_ps(GGML_CPU_FP16_TO_FP32(x[ib].d) * GGML_CPU_FP16_TO_FP32(y[ib].d));
         __m256i qx = _mm256_loadu_si256((const __m256i *)x[ib].qs);
         __m256i qy = _mm256_loadu_si256((const __m256i *)y[ib].qs);
+
+#if !__AVXVNNIINT8__
+        const __m256i y_min = _mm256_cmpeq_epi8(qy, _mm256_set1_epi8(INT8_MIN));
+        if (!_mm256_testz_si256(y_min, y_min)) {
+            ggml_vec_dot_q8_0_q8_0_generic(n, s, bs, vx, bx, vy, by, nrc);
+            return;
+        }
+#endif
 
         const __m256 q = mul_sum_i8_pairs_float(qx, qy);
 
@@ -1374,6 +1360,16 @@ void ggml_vec_dot_q8_0_q8_0(int n, float * GGML_RESTRICT s, size_t bs, const voi
         const __m128i qy_1_1 = _mm_loadu_si128((const __m128i *)y[ib].qs + 1);
         const __m128i qy_2_0 = _mm_loadu_si128((const __m128i *)y[ib + 1].qs);
         const __m128i qy_2_1 = _mm_loadu_si128((const __m128i *)y[ib + 1].qs + 1);
+
+#if !__AVXVNNIINT8__
+        const __m128i min = _mm_set1_epi8(INT8_MIN);
+        const __m128i y_min_1 = _mm_or_si128(_mm_cmpeq_epi8(qy_1_0, min), _mm_cmpeq_epi8(qy_1_1, min));
+        const __m128i y_min_2 = _mm_or_si128(_mm_cmpeq_epi8(qy_2_0, min), _mm_cmpeq_epi8(qy_2_1, min));
+        if (_mm_movemask_epi8(_mm_or_si128(y_min_1, y_min_2))) {
+            ggml_vec_dot_q8_0_q8_0_generic(n, s, bs, vx, bx, vy, by, nrc);
+            return;
+        }
+#endif
 
         const __m256 p = mul_sum_i8_quad_float(qx_1_0, qx_1_1, qx_2_0, qx_2_1, qy_1_0, qy_1_1, qy_2_0, qy_2_1);
         const __m256 deltas = quad_fp16_delta_float(x[ib].d, y[ib].d, x[ib + 1].d, y[ib + 1].d);
